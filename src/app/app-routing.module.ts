6fe724a91b25d8f12fb39e720903591b80b6f31e
Bjorn Derudder <bjorn.derudder@gmail.com>
2019-08-19 11:55:33 +0200

    Added the onboarding guard on top of the onboarding module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { OnboardingRequestResolver } from './onboarding/resolvers/onboarding-request.resolver';
+import { OnboardingGuard } from './onboarding/guards/onboarding.guard';
 
 
 const routes: Routes = [
 	{
 		path: 'onboarding',
 		resolve:{request: OnboardingRequestResolver},
+		canActivate: [OnboardingGuard],
 		loadChildren: () => import('./onboarding/onboarding.module').then((mod) => mod.OnboardingModule)
 	},
 	{
