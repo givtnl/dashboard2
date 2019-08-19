07331e9a0b82771ce2b1d611297e6ebebe35488f
Bjorn Derudder <bjorn.derudder@gmail.com>
2019-08-19 15:54:14 +0200

    Initialize the docker container

---
+++ docker/init_container.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cat >/etc/motd <<EOL 
+  _____                               
+  /  _  \ __________ _________   ____  
+ /  /_\  \\___   /  |  \_  __ \_/ __ \ 
+/    |    \/    /|  |  /|  | \/\  ___/ 
+\____|__  /_____ \____/ |__|    \___  >
+        \/      \/                  \/ 
+A P P   S E R V I C E   O N   L I N U X
+Documentation: http://aka.ms/webapp-linux
+EOL
+cat /etc/motd
+
+# Get environment variables to show up in SSH session
+eval $(printenv | awk -F= '{print "export " $1"="$2 }' >> /etc/profile)
+
+# Starting nginx
+mkdir -p /home/LogFiles/nginx
+if test ! -e /home/LogFiles/nginx/error.log; then 
+    touch /home/LogFiles/nginx/error.log
+fi
+(sleep 5; /usr/sbin/nginx) &
+(sleep 8 ; /etc/init.d/sshd restart ) &
+
+exec -c /sbin/init
