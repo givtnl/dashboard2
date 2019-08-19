0781643f68bdc8650a10f2220c0ad00612260aff
Bjorn Derudder <bjorn.derudder@gmail.com>
2019-08-19 11:54:22 +0200

    added the register onboarding model to the storage

--- src/app/onboarding/services/onboarding-state.service.ts
+++ src/app/onboarding/services/onboarding-state.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { OnboardingRequestModel } from '../models/onboarding-request.model';
+import { RegisterOnboardingModel } from '../models/register-onboarding.model';
 
 @Injectable({
 	providedIn: 'root'
@@ -13,5 +14,15 @@ export class OnboardingStateService {
     public set currentOnboardingRequest(value: OnboardingRequestModel){
         const key = 'OnboardingStateService.CurrentOnBoardingRequest';
         localStorage.setItem(key, JSON.stringify(value));
+    }
+
+    public get currentOnboardingRegistration(): RegisterOnboardingModel {
+        const key = 'OnboardingStateService.CurrentOnboardingRegistration'
+        return JSON.parse(localStorage.getItem(key))
+    }
+
+    public set currentOnboardingRegistration(value: RegisterOnboardingModel) {
+        const key = 'OnboardingStateService.CurrentOnboardingRegistration'
+        localStorage.setItem(key, JSON.stringify(value))
     }
 }
