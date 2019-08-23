8e0b5a78f6f34e192fe751e2a85e13e05e5e1e98
Anthony <anthony@givtapp.be>
2019-08-23 10:24:07 +0200

    remove unused code

--- src/app/onboarding/onboarding-completed/onboarding-completed.component.ts
+++ src/app/onboarding/onboarding-completed/onboarding-completed.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-onboarding-completed',
   templateUrl: './onboarding-completed.component.html',
   styleUrls: ['./onboarding-completed.component.scss']
 })
-export class OnboardingCompletedComponent implements OnInit {
+export class OnboardingCompletedComponent  {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
 
 }
--- src/app/onboarding/onboarding-personal-details/onboarding-personal-details.component.ts
+++ src/app/onboarding/onboarding-personal-details/onboarding-personal-details.component.ts
@@ -9,98 +9,85 @@ import { TranslateService } from '@ngx-translate/core';
 import { tap, switchMap, catchError } from 'rxjs/operators';
 
 @Component({
-	selector: 'app-onboarding-personal-details',
-	templateUrl: './onboarding-personal-details.component.html',
-	styleUrls: [ './onboarding-personal-details.component.scss' ]
+    selector: 'app-onboarding-personal-details',
+    templateUrl: './onboarding-personal-details.component.html',
+    styleUrls: ['./onboarding-personal-details.component.scss']
 })
 export class OnboardingPersonalDetailsComponent implements OnInit {
-	public form: FormGroup;
-	public loading = false;
-	constructor(
-		private translationService: TranslateService,
-		private formBuilder: FormBuilder,
-		private stateService: OnboardingStateService,
-		private router: Router,
-		private onboardingService: OnboardingService,
-		private toastr: ToastrService
-	) {}
+    public form: FormGroup;
+    public loading = false;
+    constructor(
+        private translationService: TranslateService,
+        private formBuilder: FormBuilder,
+        private stateService: OnboardingStateService,
+        private router: Router,
+        private onboardingService: OnboardingService,
+        private toastr: ToastrService
+    ) {}
 
-	ngOnInit() {
-		this.form = this.formBuilder.group({
-			firstName: [
-				this.stateService.currentRegisterModel ? this.stateService.currentRegisterModel.firstName : null,
-				[ Validators.required, Validators.minLength(2), Validators.maxLength(15) ]
-			],
-			lastName: [
-				this.stateService.currentRegisterModel ? this.stateService.currentRegisterModel.lastName : null,
-				[ Validators.required, Validators.minLength(2), Validators.maxLength(30) ]
-			]
-		});
-	}
+    ngOnInit() {
+        this.form = this.formBuilder.group({
+            firstName: [this.stateService.currentRegisterModel ? this.stateService.currentRegisterModel.firstName : null, [Validators.required, Validators.minLength(2), Validators.maxLength(15)]],
+            lastName: [this.stateService.currentRegisterModel ? this.stateService.currentRegisterModel.lastName : null, [Validators.required, Validators.minLength(2), Validators.maxLength(30)]]
+        });
+    }
 
-	submit() {
-		if (this.form.invalid) {
-			this.handleInvalidForm();
-			return;
-		}
-		const currentRegisterModel = this.stateService.currentRegisterModel;
-		currentRegisterModel.firstName = this.form.value.firstName;
-		currentRegisterModel.lastName = this.form.value.lastName;
-		currentRegisterModel.collectGroupId = this.stateService.currentOnboardingRequest.collectGroupId;
-		this.stateService.currentRegisterModel = currentRegisterModel;
+    submit() {
+        if (this.form.invalid) {
+            this.handleInvalidForm();
+            return;
+        }
+        const currentRegisterModel = this.stateService.currentRegisterModel;
+        currentRegisterModel.firstName = this.form.value.firstName;
+        currentRegisterModel.lastName = this.form.value.lastName;
+        currentRegisterModel.collectGroupId = this.stateService.currentOnboardingRequest.collectGroupId;
+        this.stateService.currentRegisterModel = currentRegisterModel;
 
-		this.onboardingService
-			.createUser(currentRegisterModel)
-			.pipe(catchError((error) => throwError(this.translationService.get('errorMessages.generic-server-error'))))
-			.subscribe(
-				(x) =>
-					this.router.navigate([ '/', 'onboarding', 'completed' ], {
-						queryParamsHandling: 'merge'
-					}),
-				(error) => this.toastr.error(error.value, 'Oops')
-			);
-	}
+        this.router.navigate(['/', 'onboarding', 'completed'], {
+            queryParamsHandling: 'merge'
+        });
+    }
 
-	handleInvalidForm() {
-		let errorMessages = new Array<Observable<string>>();
-		let resolvedErrorMessages = new Array<string>();
+    handleInvalidForm() {
+        let errorMessages = new Array<Observable<string>>();
+        let resolvedErrorMessages = new Array<string>();
 
-		const firstNameErrors = this.form.get('firstName').errors;
-		const lastNameErrors = this.form.get('lastName').errors;
+        const firstNameErrors = this.form.get('firstName').errors;
+        const lastNameErrors = this.form.get('lastName').errors;
 
-		if (firstNameErrors) {
-			if (firstNameErrors.required) {
-				errorMessages.push(this.translationService.get('errorMessages.first-name-required'));
-			}
-			if (firstNameErrors.minlength) {
-				errorMessages.push(this.translationService.get('errorMessages.first-name-min-length'));
-			}
-			if (firstNameErrors.maxlength) {
-				errorMessages.push(this.translationService.get('errorMessages.first-name-max-length'));
-			}
-		}
+        if (firstNameErrors) {
+            if (firstNameErrors.required) {
+                errorMessages.push(this.translationService.get('errorMessages.first-name-required'));
+            }
+            if (firstNameErrors.minlength) {
+                errorMessages.push(this.translationService.get('errorMessages.first-name-min-length'));
+            }
+            if (firstNameErrors.maxlength) {
+                errorMessages.push(this.translationService.get('errorMessages.first-name-max-length'));
+            }
+        }
 
-		if (lastNameErrors) {
-			if (lastNameErrors.required) {
-				errorMessages.push(this.translationService.get('errorMessages.last-name-required'));
-			}
-			if (lastNameErrors.minlength) {
-				errorMessages.push(this.translationService.get('errorMessages.last-name-min-length'));
-			}
-			if (lastNameErrors.maxlength) {
-				errorMessages.push(this.translationService.get('errorMessages.last-name-max-length'));
-			}
-		}
-		this.loading = true;
-		forkJoin(errorMessages)
-			.pipe(tap((results) => (resolvedErrorMessages = results)))
-			.pipe(tap((results) => console.log(results)))
-			.pipe(switchMap((results) => this.translationService.get('errorMessages.validation-errors')))
-			.subscribe((title) =>
-				this.toastr.warning(resolvedErrorMessages.join('<br>'), title, {
-					enableHtml: true
-				})
-			)
-			.add(() => (this.loading = false));
-	}
+        if (lastNameErrors) {
+            if (lastNameErrors.required) {
+                errorMessages.push(this.translationService.get('errorMessages.last-name-required'));
+            }
+            if (lastNameErrors.minlength) {
+                errorMessages.push(this.translationService.get('errorMessages.last-name-min-length'));
+            }
+            if (lastNameErrors.maxlength) {
+                errorMessages.push(this.translationService.get('errorMessages.last-name-max-length'));
+            }
+        }
+        this.loading = true;
+        forkJoin(errorMessages)
+            .pipe(tap(results => (resolvedErrorMessages = results)))
+            .pipe(tap(results => console.log(results)))
+            .pipe(switchMap(results => this.translationService.get('errorMessages.validation-errors')))
+            .subscribe(title =>
+                this.toastr.warning(resolvedErrorMessages.join('<br>'), title, {
+                    enableHtml: true
+                })
+            )
+            .add(() => (this.loading = false));
+    }
 }
