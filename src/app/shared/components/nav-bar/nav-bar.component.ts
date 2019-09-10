fa6cb14b29969457d7ed7c52ed7954ed7665b6f6
Anthony <anthony@boutonanthony.be>
2019-09-10 15:01:35 +0200

    add classes for navbar & sidebar

---
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss']
+})
+export class NavBarComponent implements OnInit {
+
+  public showSideBar = false;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
