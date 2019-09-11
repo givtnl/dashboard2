037c8c0a120459cd6ad08a7385c88d13d393994a
Anthony <anthony@boutonanthony.be>
2019-09-11 17:35:55 +0200

    put the catch higher in the chain in the guard

--- src/app/onboarding/guards/onboarding-complete-check-success.guard.ts
+++ src/app/onboarding/guards/onboarding-complete-check-success.guard.ts
@@ -5,43 +5,48 @@ import { OnboardingService } from '../services/onboarding.service';
 import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class OnboardingCompleteCheckSuccessGuard implements CanActivate {
-    constructor(private router: Router, private onboardingService: OnboardingService, private onboardingStateService: OnboardingStateService) {}
+  constructor(
+    private router: Router,
+    private onboardingService: OnboardingService,
+    private onboardingStateService: OnboardingStateService
+  ) {}
 
-    async canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
-        const registration = this.onboardingStateService.currentRegisterModel;
-        const onboardingRequest = this.onboardingStateService.currentOnboardingRequest;
-        // manually assign these values everytime as they might get wiped out
-        registration.collectGroupId = onboardingRequest.collectGroupId;
-        // using a promise here for readability in the beginningngnging
-        try {
-            await this.onboardingService.createUser(registration).toPromise();
-            this.onboardingStateService.clear();
-        } catch (error) {
-            if (error instanceof HttpErrorResponse) {
-                switch (error.status) {
-                    // claim or user already exists
-                    case 409:
-                        break;
-                    default:
-                        return this.HandleFailure(next);
-                }
-            } else {
-                return this.HandleFailure(next);
-            }
+  async canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
+    try {
+      const registration = this.onboardingStateService.currentRegisterModel;
+      const onboardingRequest = this.onboardingStateService.currentOnboardingRequest;
+      // manually assign these values everytime as they might get wiped out
+      registration.collectGroupId = onboardingRequest.collectGroupId;
+      // using a promise here for readability in the beginningngnging
+
+      await this.onboardingService.createUser(registration).toPromise();
+      this.onboardingStateService.clear();
+    } catch (error) {
+      if (error instanceof HttpErrorResponse) {
+        switch (error.status) {
+          // claim or user already exists
+          case 409:
+            break;
+          default:
+            return this.HandleFailure(next);
         }
-
-        this.onboardingStateService.clear();
-        return true;
+      } else {
+        return this.HandleFailure(next);
+      }
     }
 
-    private HandleFailure(next: ActivatedRouteSnapshot): boolean {
-        this.router.navigate(['/system/error-page'], {
-            queryParams: next.queryParams
-        });
+    this.onboardingStateService.clear();
+    return true;
+  }
 
-        return false;
-    }
+  private HandleFailure(next: ActivatedRouteSnapshot): boolean {
+    this.router.navigate(['/system/error-page'], {
+      queryParams: next.queryParams
+    });
+
+    return false;
+  }
 }
