3d6ebcccf7ce182a87dfde959082b6f3cca17e6e
Anthony <anthony@boutonanthony.be>
2019-09-11 17:33:50 +0200

    fix the welcome component after fixing css

--- src/app/onboarding/onboarding-welcome/onboarding-welcome.component.ts
+++ src/app/onboarding/onboarding-welcome/onboarding-welcome.component.ts
@@ -10,83 +10,90 @@ import { tap, switchMap } from 'rxjs/operators';
 import { UserRegistrationResponseModel } from '../models/user-registration-response.model';
 
 @Component({
-    selector: 'app-onboarding-welcome',
-    templateUrl: './onboarding-welcome.component.html',
-    styleUrls: ['../onboarding.module.scss', './onboarding-welcome.component.scss']
+  selector: 'app-onboarding-welcome',
+  templateUrl: './onboarding-welcome.component.html',
+  styleUrls: ['../onboarding.module.scss', './onboarding-welcome.component.scss']
 })
 export class OnboardingWelcomeComponent implements OnInit {
-    public form: FormGroup;
+  public form: FormGroup;
 
-    public get showPassword(): boolean {
-        return this.stateService.currentPreparationModel && this.stateService.currentPreparationModel.RequiredInputs && this.stateService.currentPreparationModel.RequiredInputs.some(x => x === 'Password');
+  public get showPassword(): boolean {
+    return (
+      this.stateService.currentPreparationModel &&
+      this.stateService.currentPreparationModel.RequiredInputs &&
+      this.stateService.currentPreparationModel.RequiredInputs.some(x => x === 'Password')
+    );
+  }
+
+  constructor(
+    private translationService: TranslateService,
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private toastr: ToastrService,
+    private router: Router,
+    public stateService: OnboardingStateService
+  ) {}
+
+  ngOnInit() {
+    this.stateService.currentPreparationModel = this.route.parent.snapshot.data.preparation;
+    this.stateService.currentOnboardingRequest = this.route.parent.snapshot.data.request;
+
+    this.form = this.formBuilder.group({
+      email: [{
+          value:this.stateService.currentOnboardingRequest.emailAddress,
+          disabled:true
+      }, [Validators.required, Validators.email]],
+      password: [null, this.showPassword ? [Validators.required, Validators.minLength(7)] : []]
+    });
+  }
+
+  submit() {
+    if (this.form.invalid) {
+      this.handleInvalidForm();
+      return;
+    }
+    const currentRegisterModel = this.stateService.currentRegisterModel || { email: null, password: null };
+    currentRegisterModel.email = this.form.getRawValue().email;
+    currentRegisterModel.password = this.form.value.password;
+
+    this.stateService.currentRegisterModel = currentRegisterModel;
+    this.router.navigate(['/', 'onboarding', 'welcome', 'new-users', { outlets: { 'onboarding-outlet': ['register'] } }], {
+      queryParamsHandling: 'merge'
+    });
+  }
+
+  handleInvalidForm() {
+    let errorMessages = new Array<Observable<string>>();
+    let resolvedErrorMessages = new Array<string>();
+
+    const emailErrors = this.form.get('email').errors;
+    const passwordErrors = this.form.get('password').errors;
+
+    if (emailErrors) {
+      if (emailErrors.required) {
+        errorMessages.push(this.translationService.get('errorMessages.email-required'));
+      }
+      if (emailErrors.email) {
+        errorMessages.push(this.translationService.get('errorMessages.email-not-an-email'));
+      }
     }
 
-    constructor(
-        private translationService: TranslateService,
-        private formBuilder: FormBuilder,
-        private route: ActivatedRoute,
-        private toastr: ToastrService,
-        private router: Router,
-        public stateService: OnboardingStateService
-    ) {}
-
-    ngOnInit() {
-        this.stateService.currentPreparationModel = this.route.parent.snapshot.data.preparation;
-        this.stateService.currentOnboardingRequest = this.route.parent.snapshot.data.request;
-
-        this.form = this.formBuilder.group({
-            email: [this.route.snapshot.queryParams.emailAddress, [Validators.required, Validators.email]],
-            password: [this.route.snapshot.queryParams.password, this.showPassword ? [Validators.required, Validators.minLength(7)] : []]
-        });
+    if (passwordErrors) {
+      if (passwordErrors.required) {
+        errorMessages.push(this.translationService.get('errorMessages.password-required'));
+      }
+      if (passwordErrors.minlength) {
+        errorMessages.push(this.translationService.get('errorMessages.password-min-length'));
+      }
     }
-
-    submit() {
-        if (this.form.invalid) {
-            this.handleInvalidForm();
-            return;
-        }
-        const currentRegisterModel = this.stateService.currentRegisterModel || { email: null, password: null };
-        currentRegisterModel.email = this.form.value.email;
-        currentRegisterModel.password = this.form.value.password;
-
-        this.stateService.currentRegisterModel = currentRegisterModel;
-        this.router.navigate(['/', 'onboarding', 'register'], {
-            queryParamsHandling: 'merge'
-        });
-    }
-
-    handleInvalidForm() {
-        let errorMessages = new Array<Observable<string>>();
-        let resolvedErrorMessages = new Array<string>();
-
-        const emailErrors = this.form.get('email').errors;
-        const passwordErrors = this.form.get('password').errors;
-
-        if (emailErrors) {
-            if (emailErrors.required) {
-                errorMessages.push(this.translationService.get('errorMessages.email-required'));
-            }
-            if (emailErrors.email) {
-                errorMessages.push(this.translationService.get('errorMessages.email-not-an-email'));
-            }
-        }
-
-        if (passwordErrors) {
-            if (passwordErrors.required) {
-                errorMessages.push(this.translationService.get('errorMessages.password-required'));
-            }
-            if (passwordErrors.minlength) {
-                errorMessages.push(this.translationService.get('errorMessages.password-min-length'));
-            }
-        }
-        forkJoin(errorMessages)
-            .pipe(tap(results => (resolvedErrorMessages = results)))
-            .pipe(tap(results => console.log(results)))
-            .pipe(switchMap(results => this.translationService.get('errorMessages.validation-errors')))
-            .subscribe(title =>
-                this.toastr.warning(resolvedErrorMessages.join('<br>'), title, {
-                    enableHtml: true
-                })
-            );
-    }
+    forkJoin(errorMessages)
+      .pipe(tap(results => (resolvedErrorMessages = results)))
+      .pipe(tap(results => console.log(results)))
+      .pipe(switchMap(results => this.translationService.get('errorMessages.validation-errors')))
+      .subscribe(title =>
+        this.toastr.warning(resolvedErrorMessages.join('<br>'), title, {
+          enableHtml: true
+        })
+      );
+  }
 }
