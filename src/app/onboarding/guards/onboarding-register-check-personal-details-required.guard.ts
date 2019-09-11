2b3d685fd2a8388ed17c07e16b48f0f943fcc764
Anthony <anthony@boutonanthony.be>
2019-09-11 17:36:08 +0200

    fix personal details guard to be conform new routing

--- src/app/onboarding/guards/onboarding-register-check-personal-details-required.guard.ts
+++ src/app/onboarding/guards/onboarding-register-check-personal-details-required.guard.ts
@@ -3,24 +3,23 @@ import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from
 import { OnboardingStateService } from '../services/onboarding-state.service';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class OnboardingRegisterCheckPersonalDetailsRequiredGuard implements CanActivate {
-    constructor(private router: Router, private onboardingStateService: OnboardingStateService) {}
+  constructor(private router: Router, private onboardingStateService: OnboardingStateService) {}
 
-    canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-        
-        const preparationModel = this.onboardingStateService.currentPreparationModel;
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const preparationModel = this.onboardingStateService.currentPreparationModel;
 
-        const personalDetailsRequired = preparationModel.RequiredInputs.some(x => x === 'LastName' || x === 'FirstName');
+    const personalDetailsRequired = preparationModel.RequiredInputs.some(x => x === 'LastName' || x === 'FirstName');
 
-        if (personalDetailsRequired) {
-            return true;
-        } else {
-            this.router.navigate(['/','onboarding','completed'], {
-                queryParams: next.queryParams
-            });
-            return false;
-        }
+    if (personalDetailsRequired) {
+      return true;
+    } else {
+      this.router.navigate(['/', 'onboarding', 'welcome', 'new-users', { outlets: { 'onboarding-outlet': ['completed'] } }], {
+        queryParams: next.queryParams
+      });
+      return false;
     }
+  }
 }
