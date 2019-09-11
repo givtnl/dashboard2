a596d0fa2efd825f2e11c0374af612fec91bfece
Anthony <anthony@boutonanthony.be>
2019-09-11 17:36:58 +0200

    remove the toastr from the guard

--- src/app/onboarding/guards/onboarding.guard.ts
+++ src/app/onboarding/guards/onboarding.guard.ts
@@ -2,26 +2,19 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from
 import { Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 
-@Injectable ({
-    providedIn: 'root'
+@Injectable({
+  providedIn: 'root'
 })
 export class OnboardingGuard implements CanActivate {
+  constructor(private router: Router) {}
 
-    constructor(private router: Router, private toastr: ToastrService) {}
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const token = next.queryParams.token as string;
+    const companyName = next.queryParams.cgname as string;
 
-    canActivate(
-        next: ActivatedRouteSnapshot,
-        state: RouterStateSnapshot
-    ): boolean
-    {
-        const token = next.queryParams.token as string;
-        const companyName = next.queryParams.cgname as string;
-        
-		if (!token || !companyName) {
-            this.router.navigate([ '/', 'system', 'not-found' ]);
-            this.toastr.warning('Oooops', ' todo translate : Ongeldige query params');
-            return false
-        } else 
-            return true
-    }
+    if (!token || !companyName) {
+      this.router.navigate(['/', 'system', 'not-found']);
+      return false;
+    } else return true;
+  }
 }
