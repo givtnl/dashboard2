a2820fd0d21f6a44c48058b06135bed5031ab244
Anthony <anthony@boutonanthony.be>
2019-09-12 14:25:31 +0200

    clear out the dashboard component

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,11 +5,12 @@ import { SharedModule } from '../shared/shared.module';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardRootComponent } from './dashboard-root/dashboard-root.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { DashboardCompleteAccountWidgetComponent } from './components/dashboard-complete-account-widget/dashboard-complete-account-widget.component';
 
 
 
 @NgModule({
-  declarations: [ DashboardHomeComponent, DashboardRootComponent],
+  declarations: [ DashboardHomeComponent, DashboardRootComponent, DashboardCompleteAccountWidgetComponent],
   imports: [
     DashboardRoutingModule,
     CommonModule,
