4522035bc6f285e7bf29aacb57ccaead3fa2d5fd
Jonas Brabant <jonas@givtapp.net>
2019-09-12 15:00:14 +0200

    Fix Tony-mistake - add ,

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://givtapicorewindbg.azurewebsites.net'
+  apiUrl: 'https://givtapicorewindbg.azurewebsites.net',
   oldDashboardUrl:'https://cloud.givtapp.net'
 };
 
