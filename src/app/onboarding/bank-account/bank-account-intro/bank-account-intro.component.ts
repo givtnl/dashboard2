b73265023472ba79cb7d5951e6e46404ecb52271
Jonas Brabant <jonas@givtapp.net>
2019-09-12 11:19:55 +0200

    Style Bank account intro + create component bank account intro

--- src/app/onboarding/bank-account/bank-account-intro/bank-account-intro.component.ts
+++ src/app/onboarding/bank-account/bank-account-intro/bank-account-intro.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-bank-account-intro',
   templateUrl: './bank-account-intro.component.html',
-  styleUrls: ['./bank-account-intro.component.scss']
+  styleUrls: ['../../onboarding.module.scss', './bank-account-intro.component.scss']
 })
 export class BankAccountIntroComponent implements OnInit {
 
