58743d8bb0c69120544ba5e843e6a6b36c0fb830
Anthony <anthony@boutonanthony.be>
2019-09-13 15:13:33 +0200

    finishing up the complete account widget
    a

---
+++ src/app/dashboard/components/dashboard-complete-account-widget/models/complete-account-widget.model.ts
@@ -0,0 +1,7 @@
+export interface CompleteAccountWidgetModel {
+    DisplayOrder:number;
+    Finished:boolean;
+    InProgress:boolean;
+    OrganisationRegistrationStatus:number;
+    OrganisationRegistrationStatusDescription:string;
+}
