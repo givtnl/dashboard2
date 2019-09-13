e7167764ce4416fe077f44aaaced320018bb2b01
Mike Pattyn <mike@givtapp.net>
2019-09-13 13:21:44 +0200

    added root component for system

--- src/app/system/system-routing.module.ts
+++ src/app/system/system-routing.module.ts
@@ -2,16 +2,30 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { NgModule } from '@angular/core';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { SystemRootComponent } from './system-root/system-root.component';
 
 const routes: Routes = [
 	{
-		path: 'not-found',
-		component: NotFoundComponent
+		path: 'root',
+		component: SystemRootComponent,
+		children: [
+			{
+				path: 'error',
+				component: ErrorPageComponent,
+				outlet: 'system-outlet'
+			},
+			{
+				path: 'not-found',
+				component: NotFoundComponent,
+				outlet: 'system-outlet'
+			}
+		]
 	}
 ];
 
 @NgModule({
-	imports: [ RouterModule.forChild(routes) ],
-	exports: [ RouterModule ]
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule]
 })
-export class SystemRoutingModule {}
+export class SystemRoutingModule { }
--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -4,11 +4,16 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { SystemRoutingModule } from './system-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { ErrorPageComponent } from './error-page/error-page.component';
+import { SystemRootComponent } from './system-root/system-root.component';
 
 
 
 @NgModule({
-  declarations: [NotFoundComponent, ErrorPageComponent],
+  declarations: [
+    NotFoundComponent, 
+    ErrorPageComponent, 
+    SystemRootComponent
+  ],
   imports: [
     CommonModule,
     SharedModule,
