8b461c933c86dff211c86b16020cfba7970a5c2b
Jonas Brabant <jonas@givtapp.net>
2019-09-16 20:08:14 +0200

    Styling bank account completed

--- src/app/onboarding/bank-account/onboarding-bank-account-completed/onboarding-bank-account-completed.component.ts
+++ src/app/onboarding/bank-account/onboarding-bank-account-completed/onboarding-bank-account-completed.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-onboarding-bank-account-completed',
   templateUrl: './onboarding-bank-account-completed.component.html',
-  styleUrls: ['./onboarding-bank-account-completed.component.scss']
+  styleUrls: ['../../onboarding.module.scss', './onboarding-bank-account-completed.component.scss']
 })
 export class OnboardingBankAccountCompletedComponent implements OnInit {
 
