d0f4122bcb5b74dac0bb1b4eee9ae1d46599b594
Anthony <anthony@boutonanthony.be>
2019-09-16 21:29:38 +0200

    add

--- src/app/onboarding/bank-account/models/add-bank-account-to-organisation.command.ts
+++ src/app/onboarding/bank-account/models/add-bank-account-to-organisation.command.ts
@@ -1,6 +1,5 @@
 interface AddBankAccountToOrganisationCommand {
     name: string
-    IBAN?: string
     sortCode?: string
     accountNumber?: string
 }
--- src/app/onboarding/bank-account/onboarding-bank-account-add/onboarding-bank-account-add.component.ts
+++ src/app/onboarding/bank-account/onboarding-bank-account-add/onboarding-bank-account-add.component.ts
@@ -6,6 +6,7 @@ import { Observable, forkJoin } from 'rxjs';
 import { tap, switchMap } from 'rxjs/operators';
 import { ToastrService } from 'ngx-toastr';
 import { fixedLengthValidator } from '../../../shared/validators/fixed-length.validator';
+import { OnboardingBankAccountStateService } from '../services/onboarding-bank-account-state.service';
 
 @Component({
   selector: 'app-onboarding-bank-account-add',
@@ -16,19 +17,17 @@ export class OnboardingBankAccountAddComponent implements OnInit {
   public orgCountry: string = 'GB';
 
   public form: FormGroup;
-  public loading = false;
 
   constructor(
     private translationService: TranslateService,
     private formBuilder: FormBuilder,
     private toastr: ToastrService,
     private router: Router,
-
+    private stateService: OnboardingBankAccountStateService
   ) {}
 
   ngOnInit() {
     this.form = this.formBuilder.group({
-      iban: [null, [Validators.required, Validators.minLength(12), Validators.maxLength(34)]],
       sortCode: [null, [Validators.required, fixedLengthValidator(6)]],
       accountNumber: [null, [Validators.required, fixedLengthValidator(8)]],
       name: [null, [Validators.required]]
@@ -40,11 +39,8 @@ export class OnboardingBankAccountAddComponent implements OnInit {
       this.handleInvalidForm();
       return;
     }
-    this.loading = true;
-    // this.service
-    //   .addBankAccount(this.form.value)
-    //   .subscribe(x => this.router.navigate(['/', 'onboarding', 'bank-account', { outlets: { 'onboarding-outlet': ['authorized'] } }]))
-    //   .add(() => (this.loading = false));
+    this.stateService.currentBankAccountModel = this.form.value;
+    this.router.navigate(['/', 'onboarding', 'bank-account', { outlets: { 'onboarding-outlet': ['authorized'] } }]);
   }
 
   handleInvalidForm() {
@@ -52,18 +48,6 @@ export class OnboardingBankAccountAddComponent implements OnInit {
     let resolvedErrorMessages = new Array<string>();
 
     const nameErrors = this.form.get('name').errors;
-    const ibanErrors = this.form.get('iban').errors;
-    if (ibanErrors) {
-      if (ibanErrors.required) {
-        errorMessages.push(this.translationService.get('errorMessages.iban-required'));
-      }
-      if (ibanErrors.minlength) {
-        errorMessages.push(this.translationService.get('errorMessages.iban-min-length'));
-      }
-      if (ibanErrors.maxlength) {
-        errorMessages.push(this.translationService.get('errorMessages.iban-max-length'));
-      }
-    }
     const sortCodeErrors = this.form.get('sortCode').errors;
     const accountNumberErrors = this.form.get('accountNumber').errors;
 
