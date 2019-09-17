2fb3118375668f712d5e0ed4adf9fbb4dfd5c83a
Anthony <anthony@boutonanthony.be>
2019-09-17 16:59:58 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { HttpClientModule, HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateModule, TranslateLoader, MissingTranslationHandler } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { UrlSerializer } from '@angular/router';
 import { CustomUrlSerializer } from './shared/serializers/custom-url.serializer';
 import { OnboardingInterceptor } from './onboarding/new-users/interceptors/onboarding.interceptor';
+import { ApplicationLoadService } from './infrastructure/services/application-load.service';
+import { MissingFileTranslationsHandler } from './infrastructure/services/missing-file-translations.service';
+
+export function init_app(appLoadService: ApplicationLoadService) {
+  return () => appLoadService.initializeApp();
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -23,6 +29,7 @@ import { OnboardingInterceptor } from './onboarding/new-users/interceptors/onboa
       preventDuplicates: true
     }),
     TranslateModule.forRoot({
+      missingTranslationHandler: { provide: MissingTranslationHandler, useClass: MissingFileTranslationsHandler },
       loader: {
         provide: TranslateLoader,
         useFactory: httpClient => new TranslateHttpLoader(httpClient),
@@ -33,6 +40,12 @@ import { OnboardingInterceptor } from './onboarding/new-users/interceptors/onboa
   ],
   bootstrap: [AppComponent],
   providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: init_app,
+      deps: [ApplicationLoadService],
+      multi: true
+    },
     {
       provide: HTTP_INTERCEPTORS,
       useClass: OnboardingInterceptor,
