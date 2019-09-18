effe848763e71237a449ee1acf39df4664277100
Anthony <anthony@boutonanthony.be>
2019-09-18 10:47:12 +0200

    prevent duplicates when no translations where found

--- src/app/infrastructure/services/missing-file-translations.service.ts
+++ src/app/infrastructure/services/missing-file-translations.service.ts
@@ -6,17 +6,17 @@ import { of, Observable } from 'rxjs';
 export class MissingFileTranslationsHandler implements MissingTranslationHandler {
   handle(params: MissingTranslationHandlerParams): Observable<string> {
     if (!params || !params.key) {
-      return of(`No translation found for key ${params.key} for language ${params.translateService.currentLang}`);
+      return of();
     }
     // we dont care if uppercase or lowercase
     var currentLanguageTranslations = params.translateService.translations[params.translateService.currentLang];
     if (!currentLanguageTranslations) {
-      return of(`No translation found for key ${params.key} for language ${params.translateService.currentLang}`);
+      return of();
     }
 
     var matchedKey = Object.keys(currentLanguageTranslations).find(key => key.toLowerCase() === params.key.toLowerCase());
     if (!matchedKey) {
-      return of(`No translation found for key ${params.key} for language ${params.translateService.currentLang}`);
+      return of();
     }
 
     return of(currentLanguageTranslations[matchedKey]);
