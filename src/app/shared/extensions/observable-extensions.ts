3e5d5154f167225d8cf0d3f3b1303703fdfc22aa
Anthony <anthony@boutonanthony.be>
2019-09-18 11:46:27 +0200

    provide the callback method with the method

--- src/app/shared/extensions/observable-extensions.ts
+++ src/app/shared/extensions/observable-extensions.ts
@@ -5,8 +5,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 export function catchErrorStatus<T>(status: number, callback: any): OperatorFunction<T, T> {
   return catchError((error: HttpErrorResponse) => {
     if (error.status === status) {
-      console.warn('Catched 401 error');
-      callback();
+      callback(error);
     }
     return throwError(error);
   });
