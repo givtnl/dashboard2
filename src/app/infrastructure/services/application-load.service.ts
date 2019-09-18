23ca643b9a2c6557515bcf76081a3e4475bd2720
Anthony <anthony@boutonanthony.be>
2019-09-18 16:52:13 +0200

    remove redudant code

--- src/app/infrastructure/services/application-load.service.ts
+++ src/app/infrastructure/services/application-load.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { ApplicationStateService } from './application-state.service';
-import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,13 +8,6 @@ export class ApplicationLoadService {
   constructor(private applicationStateService: ApplicationStateService) {}
 
   public initializeApp() {
-    if (!environment.production) {
-      this.applicationStateService.currentUserModel = {
-        organisationId: '0A5114B3-D21E-4615-BC1E-0853240EFBEE',
-        organisationName: 'Dorcas',
-        userName: 'Debug gebruiker',
-        userEmail: 'Debugemail@debug.debug'
-      };
-    }
+  
   }
 }
