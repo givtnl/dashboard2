21872104caaf3af36b6ea5a69b26c9d6a3a51b75
Anthony <anthony@boutonanthony.be>
2019-09-18 12:43:59 +0200

    Create onboarding-bank-account-complete-check-success.guard.ts

---
+++ src/app/onboarding/bank-account/guards/onboarding-bank-account-complete-check-success.guard.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { OnboardingBankAccountService } from '../services/onboarding-bank-account.service';
+import { OnboardingBankAccountStateService } from '../services/onboarding-bank-account-state.service';
+import { ApplicationLoadService } from 'src/app/infrastructure/services/application-load.service';
+import { ApplicationStateService } from 'src/app/infrastructure/services/application-state.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OnboardingBankAccountCompleteCheckSuccessGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private applicationStateService: ApplicationStateService,
+    private onboardingBankAccountService: OnboardingBankAccountService,
+    private onboardingBankAccountStateService: OnboardingBankAccountStateService
+  ) {}
+
+  async canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
+    try {
+      const registration = this.onboardingBankAccountStateService.currentBankAccountModel;
+      const currentUser = this.applicationStateService.currentUserModel;
+
+      await this.onboardingBankAccountService.create(currentUser.organisationId, registration).toPromise();
+      this.onboardingBankAccountStateService.clear();
+    } catch (error) {
+      if (error instanceof HttpErrorResponse) {
+        return this.HandleFailure(next);
+      }
+    }
+
+    this.onboardingBankAccountStateService.clear();
+    return true;
+  }
+
+  private HandleFailure(next: ActivatedRouteSnapshot): boolean {
+    this.router.navigate(['/system/error-page'], {
+      queryParams: next.queryParams
+    });
+
+    return false;
+  }
+}
