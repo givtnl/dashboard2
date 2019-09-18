978f2f27013fef64e158e215a92bb27764272585
Anthony <anthony@boutonanthony.be>
2019-09-18 10:47:58 +0200

    add a method to get the current registrationstatus

--- src/app/onboarding/bank-account/services/onboarding-bank-account.service.ts
+++ src/app/onboarding/bank-account/services/onboarding-bank-account.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BackendService } from 'src/app/infrastructure/services/backend.service';
 import { Observable } from 'rxjs';
+import { OnboardingBankAccountRegistrationResponseModel } from '../models/onboarding-bank-account-registration-response.model';
 
 @Injectable({
     providedIn:'root'
@@ -8,7 +9,11 @@ import { Observable } from 'rxjs';
 export class OnboardingBankAccountService {
     constructor(private backendService: BackendService) {}
 
+    getRegistrationStatus(organisationId: string): Observable<OnboardingBankAccountRegistrationResponseModel> {
+        return this.backendService.get(`v2/organisations/${organisationId}/registration/account`);
+    }
+
     create(command: AddBankAccountToOrganisationCommand): Observable<object>{
-        return this.backendService.post(`organisations/${this.backendService.currentUser.organisationId}/accounts`, command);
+        return this.backendService.post(`organisations/${command.organisationId}/accounts`, command);
     }
 }
