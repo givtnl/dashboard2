a153ed9526237c3bdf463facb02357b1052e16c7
Jonas Brabant <jonas@givtapp.net>
2019-09-18 16:13:03 +0200

    validation and error messages

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -1,67 +1,117 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { TranslateService } from '@ngx-translate/core';
-import { AccountService } from '../services/account.service';
-import { Observable } from 'rxjs';
-import { catchErrorStatus } from 'src/app/shared/extensions/observable-extensions';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { TranslateService } from "@ngx-translate/core";
+import { AccountService } from "../services/account.service";
+import { Observable } from "rxjs";
+import { catchErrorStatus } from "src/app/shared/extensions/observable-extensions";
+import { HttpErrorResponse } from "@angular/common/http";
+import { ErrorMessages } from "src/app/infrastructure/enums/error-messages.enum";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
   public form: FormGroup;
-  public loading = false;
+  public submitted = false;
+  
+  public isValidEmail = false;
+  public isValidPassword = false;
 
-  constructor(private fb: FormBuilder,private router: Router, private translationService: TranslateService, private accountService: AccountService) {}
+  public errorMessages = null;
+
+  constructor(
+    private fb: FormBuilder,
+    private translationService: TranslateService,
+    private accountService: AccountService
+  ) {}
 
   ngOnInit() {
     this.form = this.fb.group({
       email: [null, [Validators.required, Validators.email]],
       password: [null, [Validators.required]]
     });
-  }
+      }
 
   submit() {
+    this.submitted = this.form.invalid;
+
+    this.isValidEmail = this.form.get('email').valid;
+    this.isValidPassword = this.form.get('password').valid;
+
+    this.errorMessages = new Array<Observable<string>>();
+
     if (this.form.invalid) {
       this.handleInvalidForm();
       return;
     } else {
-      this.loading = true;
+      this.submitted = false;
       this.accountService
         .login(this.form.value.email, this.form.value.password)
         .pipe(catchErrorStatus(400, this.handleInvalidLogin))
-        .subscribe(resp => this.router.navigate(['/','dashboard','root', {outlets:{'dashboard-outlet':['home']}}]))
-        .add(() => (this.loading = false));
+        .subscribe(resp => {
+          console.log(resp);
+        });
     }
   }
-  handleInvalidLogin() {
-    console.log('invalid login');
+  handleInvalidLogin(error: HttpErrorResponse) {
+    switch (error.error.error_status) {
+      case ErrorMessages.AccountDisabled:
+        {
+        }
+        break;
+      case ErrorMessages.LockedOut:
+        {
+        }
+        break;
+      case ErrorMessages.OneAttemptLeft:
+        {
+        }
+        break;
+      case ErrorMessages.TwoAttemptsLeft:
+        {
+        }
+        break;
+      case ErrorMessages.WrongPassOrUser:
+        {
+          this.form.get("password");
+        }
+        break;
+      default:
+        return;
+    }
   }
   handleInvalidForm() {
-    let errorMessages = new Array<Observable<string>>();
-    let resolvedErrorMessages = new Array<string>();
-
-    const emailErrors = this.form.get('email').errors;
-    const passwordErrors = this.form.get('password').errors;
+    const emailErrors = this.form.get("email").errors;
+    const passwordErrors = this.form.get("password").errors;
 
     if (emailErrors) {
+     
       if (emailErrors.required) {
-        errorMessages.push(this.translationService.get('errorMessages.email-required'));
+        this.errorMessages.push(
+          this.translationService.get("errorMessages.email-required")
+        );
       }
       if (emailErrors.email) {
-        errorMessages.push(this.translationService.get('errorMessages.email-not-an-email'));
+        this.errorMessages.push(
+          this.translationService.get("errorMessages.email-not-an-email")
+        );
       }
     }
 
     if (passwordErrors) {
+ 
       if (passwordErrors.required) {
-        errorMessages.push(this.translationService.get('errorMessages.password-required'));
+        console.log("hoi");
+        this.errorMessages.push(
+          this.translationService.get("errorMessages.password-required")
+        );
       }
       if (passwordErrors.minlength) {
-        errorMessages.push(this.translationService.get('errorMessages.password-min-length'));
+        this.errorMessages.push(
+          this.translationService.get("errorMessages.password-min-length")
+        );
       }
     }
   }
