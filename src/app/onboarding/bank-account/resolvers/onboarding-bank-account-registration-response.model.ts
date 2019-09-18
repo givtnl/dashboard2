16c35e57e1bf4cdec251439f896af3a4e3e39161
Anthony <anthony@boutonanthony.be>
2019-09-18 10:47:46 +0200

    add a resolver to fetch our current details

---
+++ src/app/onboarding/bank-account/resolvers/onboarding-bank-account-registration-response.model.ts
@@ -0,0 +1,26 @@
+import { OnboardingBankAccountService } from '../services/onboarding-bank-account.service';
+import { ApplicationStateService } from 'src/app/infrastructure/services/application-state.service';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import {OnboardingBankAccountRegistrationResponseModel} from '../models/onboarding-bank-account-registration-response.model';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn:'root'
+})
+export class OnboardingBankAccountRegistrationResolver implements Resolve<OnboardingBankAccountRegistrationResponseModel> {
+  /**
+   *
+   */
+  constructor(private service: OnboardingBankAccountService, private applicationStateService: ApplicationStateService) {}
+
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | OnboardingBankAccountRegistrationResponseModel
+    | Observable<OnboardingBankAccountRegistrationResponseModel>
+    | Promise<OnboardingBankAccountRegistrationResponseModel> {
+    return this.service.getRegistrationStatus(this.applicationStateService.currentUserModel.organisationId);
+  }
+}
