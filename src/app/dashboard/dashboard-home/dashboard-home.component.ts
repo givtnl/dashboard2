99a966ccb8f04136aa015487cb81aa622c05c632
Anthony <anthony@boutonanthony.be>
2019-09-19 11:08:42 +0200

    implement the collectgroup widgets on dashboard

--- src/app/dashboard/dashboard-home/dashboard-home.component.ts
+++ src/app/dashboard/dashboard-home/dashboard-home.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ApplicationStateService } from 'src/app/infrastructure/services/application-state.service';
+import { CollectGroupDashboardListModel } from './models/collect-group-side-bar-list.model';
+import { DashboardService } from './services/dashboard.service';
 
 @Component({
   selector: 'app-dashboard-home',
   templateUrl: './dashboard-home.component.html',
   styleUrls: ['./dashboard-home.component.scss']
 })
-export class DashboardHomeComponent {
-  constructor(public applicationStateService: ApplicationStateService) {}
+export class DashboardHomeComponent implements OnInit {
+  public loading = false;
+  public collectGroups = new Array<CollectGroupDashboardListModel>();
+  constructor(public applicationStateService: ApplicationStateService, private dashboardService: DashboardService) {}
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.dashboardService
+      .getCollectGroups()
+      .subscribe(x => (this.collectGroups = x))
+      .add(() => (this.loading = false));
+  }
+
+  getIconForCollectGroupType(collectGroupType: number): string {
+    switch (collectGroupType) {
+      case 0:
+        return 'fa-church';
+      case 1:
+        return 'fa-bullhorn';
+      case 2:
+        return 'fa-guitar';
+      case 3:
+        return 'fa-hand-holding-heart';
+      default:
+        return 'fa-heart';
+    }
+  }
 }
