55e55fac35b2a1abfe874b23dc02bb2f02e0a2aa
Anthony <anthony@boutonanthony.be>
2019-09-19 11:08:16 +0200

    add logout to sidebar

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -1,25 +1,27 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AccountService } from 'src/app/account/services/account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.scss']
 })
-export class SideBarComponent  {
-
+export class SideBarComponent {
   @Input()
   public showCloseButton = false;
 
   @Output()
   public closeButtonClicked = new EventEmitter();
 
-  constructor() { }
+  constructor(private accountService: AccountService, private router: Router) {}
 
-  public closeMenu():void{
+  public closeMenu(): void {
     this.closeButtonClicked.emit();
   }
 
-
- 
-
+  logOut(): void {
+    this.accountService.logOut();
+    this.router.navigate(['/', 'account', 'login']);
+  }
 }
