76990beab13d8731beadfb8f543ca93d0cc23aa2
Jonas Brabant <jonas@givtapp.net>
2019-09-19 12:03:06 +0200

    Format document

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -1,22 +1,22 @@
-import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
-import { TranslateService } from "@ngx-translate/core";
-import { AccountService } from "../services/account.service";
-import { Observable } from "rxjs";
-import { catchErrorStatus } from "src/app/shared/extensions/observable-extensions";
-import { HttpErrorResponse } from "@angular/common/http";
-import { ErrorMessages } from "src/app/infrastructure/enums/error-messages.enum";
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
+import { AccountService } from '../services/account.service';
+import { Observable } from 'rxjs';
+import { catchErrorStatus } from 'src/app/shared/extensions/observable-extensions';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ErrorMessages } from 'src/app/infrastructure/enums/error-messages.enum';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: "app-login",
-  templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.scss"]
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
   public form: FormGroup;
   public submitted = false;
-  public loading = false;  
+  public loading = false;
   public isValidEmail = false;
   public isValidPassword = false;
 
@@ -34,7 +34,7 @@ export class LoginComponent implements OnInit {
       email: [null, [Validators.required, Validators.email]],
       password: [null, [Validators.required]]
     });
-      }
+  }
 
   submit() {
     this.submitted = this.form.invalid;
@@ -50,14 +50,18 @@ export class LoginComponent implements OnInit {
     } else {
       this.loading = true;
       this.submitted = false;
-      console.log("Submitted:\n", this.errorMessages);
-
       this.accountService
         .login(this.form.value.email, this.form.value.password)
-        .pipe(catchErrorStatus(400, (x) => this.handleInvalidLogin(x)))
-        .subscribe(resp => this.router.navigate(['/','dashboard','root', {outlets:{'dashboard-outlet':['home']}}]))
+        .pipe(catchErrorStatus(400, x => this.handleInvalidLogin(x)))
+        .subscribe(resp =>
+          this.router.navigate([
+            '/',
+            'dashboard',
+            'root',
+            { outlets: { 'dashboard-outlet': ['home'] } }
+          ])
+        )
         .add(() => (this.loading = false));
-
     }
   }
   handleInvalidLogin(error: HttpErrorResponse) {
@@ -65,57 +69,57 @@ export class LoginComponent implements OnInit {
       case ErrorMessages.AccountDisabled:
         {
           this.errorMessages.push(
-            this.translationService.get("errorMessages.accountDisabled")
+            this.translationService.get('errorMessages.accountDisabled')
           );
         }
         break;
       case ErrorMessages.LockedOut:
         {
           this.errorMessages.push(
-            this.translationService.get("errorMessages.lockedOut")
+            this.translationService.get('errorMessages.lockedOut')
           );
         }
         break;
       case ErrorMessages.OneAttemptLeft:
         {
           this.errorMessages.push(
-            this.translationService.get("errorMessages.wrongEmailOrPassword")
+            this.translationService.get('errorMessages.wrongEmailOrPassword')
           );
           this.errorMessages.push(
-            this.translationService.get("errorMessages.oneAttemptLeft")
+            this.translationService.get('errorMessages.oneAttemptLeft')
           );
         }
         break;
       case ErrorMessages.TwoAttemptsLeft:
         {
           this.errorMessages.push(
-            this.translationService.get("errorMessages.wrongEmailOrPassword")
+            this.translationService.get('errorMessages.wrongEmailOrPassword')
           );
           this.errorMessages.push(
-            this.translationService.get("errorMessages.twoAttemptLeft")
+            this.translationService.get('errorMessages.twoAttemptLeft')
           );
         }
         break;
       default:
-          this.errorMessages.push(
-            this.translationService.get("errorMessages.wrongEmailOrPassword")
-          );
+        this.errorMessages.push(
+          this.translationService.get('errorMessages.wrongEmailOrPassword')
+        );
         return;
     }
   }
   handleInvalidForm() {
-    const emailErrors = this.form.get("email").errors;
-    const passwordErrors = this.form.get("password").errors;
+    const emailErrors = this.form.get('email').errors;
+    const passwordErrors = this.form.get('password').errors;
 
     if (emailErrors) {
       if (emailErrors.required) {
         this.errorMessages.push(
-          this.translationService.get("errorMessages.email-required")
+          this.translationService.get('errorMessages.email-required')
         );
       }
       if (emailErrors.email) {
         this.errorMessages.push(
-          this.translationService.get("errorMessages.email-not-an-email")
+          this.translationService.get('errorMessages.email-not-an-email')
         );
       }
     }
@@ -123,7 +127,7 @@ export class LoginComponent implements OnInit {
     if (passwordErrors) {
       if (passwordErrors.required) {
         this.errorMessages.push(
-          this.translationService.get("errorMessages.password-required")
+          this.translationService.get('errorMessages.password-required')
         );
       }
     }
