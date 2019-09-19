d3f358eeead4260515a7aef377cb3aec54124031
Mike Pattyn <mike@givtapp.net>
2019-09-19 15:26:27 +0200

    redirect to dashboard home page

--- src/app/onboarding/bank-account/onboarding-bank-account-completed/onboarding-bank-account-completed.component.ts
+++ src/app/onboarding/bank-account/onboarding-bank-account-completed/onboarding-bank-account-completed.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-onboarding-bank-account-completed',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OnboardingBankAccountCompletedComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  redirectToDashboard() {
+    this.router.navigate(['/', 'dashboard', 'root', { outlets: { 'dashboard-outlet': ['home'] } }])
+  }
 }
