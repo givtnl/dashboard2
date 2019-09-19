c42050ebbc87b71ff0bd639158871ee085f3ac45
Mike Pattyn <mike@givtapp.net>
2019-09-19 13:10:37 +0200

    added forgot password page

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { AccountRoutingModule } from './account-routing.module';
 import { SharedModule } from '../shared/shared.module';
-import { ReactiveFormsModule } from '@angular/forms'
+import { ReactiveFormsModule } from '@angular/forms';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component'
 
 
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, ForgotPasswordComponent],
   imports: [
     CommonModule,
     AccountRoutingModule,
