db4e473ad6b6e512f7bfe304acb1d0c62be8e73e
Mike Pattyn <mike@givtapp.net>
2019-09-24 15:27:27 +0200

    added form and formbuilder to code behind

--- src/app/onboarding/bank-account-holder/onboarding-bank-account-holder-who/onboarding-bank-account-holder-who.component.ts
+++ src/app/onboarding/bank-account-holder/onboarding-bank-account-holder-who/onboarding-bank-account-holder-who.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-onboarding-bank-account-holder-who',
   templateUrl: './onboarding-bank-account-holder-who.component.html',
   styleUrls: ['../../onboarding.module.scss', './onboarding-bank-account-holder-who.component.scss']
 })
+
 export class OnboardingBankAccountHolderWhoComponent implements OnInit {
 
-  constructor() { }
+  public form: FormGroup
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      firstName: [null, [Validators.required]],
+      lastName: [null, [Validators.required]]
+    })
+  }
 
   ngOnInit() {
   }
