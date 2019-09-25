422f3e20b9c98e6f10229d1de480215e5fe686cc
Anthony <anthony@boutonanthony.be>
2019-09-25 17:14:30 +0200

    extend the model we need to support the sign mandate flow

---
+++ src/app/onboarding/bank-account-signing/models/bank-account-holder-detail.model.ts
@@ -0,0 +1,15 @@
+export interface BankAccountHolderDetailModel {
+    Id: string;
+    AccountId: number;
+    FirstName: string;
+    LastName:string;
+    EmailAddress:number;
+    SortCode:string;
+    AccountNumber:string;
+    AccountName:string;
+    OrganisationId: string;
+    OrganisationName:string;
+    OrganisationStreet:string;
+    OrganisationCity:string;
+    OrganisationZipCode:string;
+}
