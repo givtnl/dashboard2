4d521f9a9726a22b0e89e300f88f2c88660ef90d
Mike Pattyn <mike@givtapp.net>
2019-09-25 13:10:38 +0200

    show different flow depending on user input

--- src/app/onboarding/bank-account-signing/onboarding-bank-account-signing-verify-details/onboarding-bank-account-signing-verify-details.component.ts
+++ src/app/onboarding/bank-account-signing/onboarding-bank-account-signing-verify-details/onboarding-bank-account-signing-verify-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-onboarding-bank-account-signing-verify-details',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../../onboarding.module.scss', './onboarding-bank-account-signing-verify-details.component.scss']
 })
 export class OnboardingBankAccountSigningVerifyDetailsComponent implements OnInit {
+  public form: FormGroup
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      detailsCorrect: [null, [Validators.required]]
+    })
+    this.form.valueChanges.subscribe(x=> {
+      if(x.detailsCorrect) {
+        this.router.navigate(['/', 'onboarding', 'bank-account-signing', { outlets: { 'onboarding-outlet': ['direct-debit-guarantee'] } }], {
+          queryParamsHandling: 'merge'
+        });
+      } else {
+        this.router.navigate(['/', 'onboarding', 'bank-account-signing', { outlets: { 'onboarding-outlet': ['details-incorrect'] } }], {
+          queryParamsHandling: 'merge'
+        });
+      }
+    })
   }
 
 }
