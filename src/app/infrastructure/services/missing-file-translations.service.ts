ecb07b00e2ed114c85a0a81e9f31d7da2fba8a42
Anthony <anthony@boutonanthony.be>
2019-09-25 11:35:23 +0200

    show no-translations

--- src/app/infrastructure/services/missing-file-translations.service.ts
+++ src/app/infrastructure/services/missing-file-translations.service.ts
@@ -6,17 +6,17 @@ import { of, Observable } from 'rxjs';
 export class MissingFileTranslationsHandler implements MissingTranslationHandler {
   handle(params: MissingTranslationHandlerParams): Observable<string> {
     if (!params || !params.key) {
-      return of();
+      return of('no_translation');
     }
     // we dont care if uppercase or lowercase
     var currentLanguageTranslations = params.translateService.translations[params.translateService.currentLang];
     if (!currentLanguageTranslations) {
-      return of();
+      return of('no_translation');
     }
 
     var matchedKey = Object.keys(currentLanguageTranslations).find(key => key.toLowerCase() === params.key.toLowerCase());
     if (!matchedKey) {
-      return of();
+      return of('no_translation');
     }
 
     return of(currentLanguageTranslations[matchedKey]);
