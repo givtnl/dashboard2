a1b54d7a869adaa2712d55c416a287264f38bd5d
Mike Pattyn <mike@givtapp.net>
2019-10-07 15:28:21 +0200

    added bankaccountype enum and setup html with 2 buttons to redirect to the old or the new dashboard

---
+++ src/app/infrastructure/enums/bank-account-type.enum.ts
@@ -0,0 +1,4 @@
+export enum BankAccountType {
+    SEPA = 0,
+    BACS = 1
+}
--- src/app/onboarding/bank-account-holder/onboarding-bank-account-holder-completed/onboarding-bank-account-holder-completed.component.ts
+++ src/app/onboarding/bank-account-holder/onboarding-bank-account-holder-completed/onboarding-bank-account-holder-completed.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApplicationStateService } from 'src/app/infrastructure/services/application-state.service';
+import { BankAccountType } from 'src/app/infrastructure/enums/bank-account-type.enum';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-onboarding-bank-account-holder-completed',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OnboardingBankAccountHolderCompletedComponent implements OnInit {
 
-  constructor() { }
+  public bankAccountType: BankAccountType
+  public oldDashboardUrl = environment.oldDashboardUrl
+
+  constructor(private stateService: ApplicationStateService) {
+
+  }
 
   ngOnInit() {
+
   }
 
 }
+
