3ebc5c10e9f14296aa87202911ab1101733a6873
Jonas Brabant <jonas@givtapp.net>
2019-10-17 10:58:54 +0200

    setup base layout for Organisation Details Verify component

--- src/app/onboarding/organisation-details/onboarding-organisation-details-verify/onboarding-organisation-details-verify.component.ts
+++ src/app/onboarding/organisation-details/onboarding-organisation-details-verify/onboarding-organisation-details-verify.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-onboarding-organisation-details-verify',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../../onboarding.module.scss', './onboarding-organisation-details-verify.component.scss']
 })
 export class OnboardingOrganisationDetailsVerifyComponent implements OnInit {
+  public form: FormGroup
+  public loading: false
 
   constructor() { }
 
