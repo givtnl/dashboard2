a4bfd0b6029c0add4323818ca9432008a8043e04
Mike Pattyn <mike@givtapp.net>
2019-10-17 11:47:15 +0200

    added logic and form to handle yes / no

--- src/app/onboarding/organisation-details/onboarding-organisation-details-verify/onboarding-organisation-details-verify.component.ts
+++ src/app/onboarding/organisation-details/onboarding-organisation-details-verify/onboarding-organisation-details-verify.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-onboarding-organisation-details-verify',
@@ -8,11 +10,32 @@ import { FormGroup } from '@angular/forms';
 })
 export class OnboardingOrganisationDetailsVerifyComponent implements OnInit {
   public form: FormGroup
-  public loading: false
-
-  constructor() { }
+  public loading = false
+  constructor(
+    private formBuilder: FormBuilder,
+    private translateService: TranslateService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      detailsCorrect: [null, [Validators.required]]
+    });
+    this.form.valueChanges.subscribe(x => {
+      if (x.detailsCorrect) {
+        this.router.navigate(
+          ['/', 'onboarding', 'organisation-details', { outlets: { 'onboarding-outlet': ['complete'] } }],
+          {
+            queryParamsHandling: 'merge'
+          }
+        );
+      } else {
+        this.loading = true
+        this.router.navigate(['/', 'onboarding', 'organisation-details', { outlets: { 'onboarding-outlet': ['incorrect'] } }], {
+          queryParamsHandling: 'merge'
+        }).finally(() => this.loading = false)
+      }
+    });
   }
 
 }
