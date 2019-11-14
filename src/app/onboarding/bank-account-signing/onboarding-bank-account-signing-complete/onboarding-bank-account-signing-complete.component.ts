8be57a73c3a5b490b71eff091afe7b3fc533079c
Anthony <anthony@boutonanthony.be>
2019-11-14 12:44:02 +0100

    dont use window.clows..never

--- src/app/onboarding/bank-account-signing/onboarding-bank-account-signing-complete/onboarding-bank-account-signing-complete.component.ts
+++ src/app/onboarding/bank-account-signing/onboarding-bank-account-signing-complete/onboarding-bank-account-signing-complete.component.ts
@@ -1,19 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-onboarding-bank-account-signing-complete',
   templateUrl: './onboarding-bank-account-signing-complete.component.html',
   styleUrls: ['../../onboarding.module.scss', './onboarding-bank-account-signing-complete.component.scss']
 })
-export class OnboardingBankAccountSigningCompleteComponent implements OnInit {
+export class OnboardingBankAccountSigningCompleteComponent {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
-
-  close() {
-    window.close();
-  }
 
 }
