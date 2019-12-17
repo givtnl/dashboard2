a698f92d416ab76b5f14993698ce808773952ea7
Anthony <anthony@boutonanthony.be>
2019-12-17 10:07:25 +0100

    add components for the password forgotten functionality

---
+++ src/app/account/password-forgotten-email-sent/password-forgotten-email-sent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-password-forgotten-email-sent',
+  templateUrl: './password-forgotten-email-sent.component.html',
+  styleUrls: ['./password-forgotten-email-sent.component.scss']
+})
+export class PasswordForgottenEmailSentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
