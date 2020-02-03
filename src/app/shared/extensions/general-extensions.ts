4506aefa2b40f72ca0fdc16df35d29270503fe75
Anthony <anthony@boutonanthony.be>
2020-02-03 14:04:42 +0100

    remove unused function

--- src/app/shared/extensions/general-extensions.ts
+++ src/app/shared/extensions/general-extensions.ts
@@ -1,13 +1,4 @@
 export class GeneralExtensions {
-  private static toCamelCase(inputString: string): string {
-    if (inputString) {
-      return inputString.replace(/\W+(.)/g, function(match, chr) {
-        return chr.toLowerCase();
-      });
-    }
-    return inputString;
-  }
-
   public static keysToCamelCase(o: any): any {
     const serializedObject = JSON.stringify(o);
     return JSON.parse(serializedObject, function(key, value) {
