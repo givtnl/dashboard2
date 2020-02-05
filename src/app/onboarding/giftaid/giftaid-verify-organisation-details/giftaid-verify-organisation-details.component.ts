30dad2e228d4a74c8d48fee0ce8699272c74093b
Mike Pattyn <mike@givtapp.net>
2020-02-05 14:51:34 +0100

    add code for the verify components

--- src/app/onboarding/giftaid/giftaid-verify-organisation-details/giftaid-verify-organisation-details.component.ts
+++ src/app/onboarding/giftaid/giftaid-verify-organisation-details/giftaid-verify-organisation-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { PreparedGiftAidSettings } from '../models/prepared-giftaid-settings.model';
 
 @Component({
   selector: 'app-giftaid-verify-organisation-details',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./giftaid-verify-organisation-details.component.scss']
 })
 export class GiftaidVerifyOrganisationDetailsComponent implements OnInit {
+  public form: FormGroup;
+  public loading = false;
+  public giftAidSettings: PreparedGiftAidSettings;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-  }
+    // setup form
+    this.form = this.formBuilder.group({
+      detailsCorrect: [null, [Validators.required]]
+    });
 
+    // load gift aid settings
+    this.giftAidSettings = this.currentSettings;
+
+    // listen to value change
+    this.form.valueChanges.subscribe(value => {
+      this.loading = true;
+      this.router.navigate(['/', 'onboarding', 'giftaid',
+        { outlets: { 'onboarding-outlet': [value.detailsCorrect ? 'completed' : 'organisation-charity-details'] } }],
+        { queryParamsHandling: 'merge' }
+      ).finally(() => this.loading = false)
+    })
+  }
+  private get currentSettings(): PreparedGiftAidSettings {
+    return this.activatedRoute.parent.snapshot.data.giftAidSettings;
+  }
 }
--- src/app/onboarding/giftaid/giftaid-verify-personal-details/giftaid-verify-personal-details.component.ts
+++ src/app/onboarding/giftaid/giftaid-verify-personal-details/giftaid-verify-personal-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { PreparedGiftAidSettings } from '../models/prepared-giftaid-settings.model';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-giftaid-verify-personal-details',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./giftaid-verify-personal-details.component.scss']
 })
 export class GiftaidVerifyPersonalDetailsComponent implements OnInit {
+  public form: FormGroup;
+  public loading = false;
+  public giftAidSettings: PreparedGiftAidSettings;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-  }
+    // setup form
+    this.form = this.formBuilder.group({
+      detailsCorrect: [null, [Validators.required]]
+    });
 
+    // load gift aid settings
+    this.giftAidSettings = this.currentSettings;
+
+    // listen to value change
+    this.form.valueChanges.subscribe(value => {
+      this.loading = true;
+      this.router.navigate(['/', 'onboarding', 'giftaid',
+        { outlets: { 'onboarding-outlet': [value.detailsCorrect ? 'completed' : 'authorised-official-details'] } }],
+        { queryParamsHandling: 'merge' }
+      ).finally(() => this.loading = false)
+    })
+  }
+  private get currentSettings(): PreparedGiftAidSettings {
+    return this.activatedRoute.parent.snapshot.data.giftAidSettings;
+  }
 }
