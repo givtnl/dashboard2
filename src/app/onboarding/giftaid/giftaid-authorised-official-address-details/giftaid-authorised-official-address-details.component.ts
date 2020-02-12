424114908a60de2cf6ee9c5a9b143f4f98eaeb48
Jonas Brabant <jonas@givtapp.net>
2020-02-12 17:39:20 +0100

    Modify and finished up official address details

--- src/app/onboarding/giftaid/giftaid-authorised-official-address-details/giftaid-authorised-official-address-details.component.ts
+++ src/app/onboarding/giftaid/giftaid-authorised-official-address-details/giftaid-authorised-official-address-details.component.ts
@@ -3,34 +3,38 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { OnboardingGiftAidStateService } from '../services/onboarding-giftaid-state.service';
 import { CreateGiftAidSettingsCommand } from '../models/create-giftaid-settings.command';
+import { ToastrService } from 'ngx-toastr';
+import { TranslateService } from '@ngx-translate/core';
+import { Observable, forkJoin } from 'rxjs';
+import { tap, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-giftaid-authorised-official-address-details',
   templateUrl: './giftaid-authorised-official-address-details.component.html',
-  styleUrls: ['./giftaid-authorised-official-address-details.component.scss']
+  styleUrls: ['../../onboarding.module.scss','./giftaid-authorised-official-address-details.component.scss']
 })
 export class GiftaidAuthorisedOfficialAddressDetailsComponent implements OnInit {
   public form: FormGroup;
 
-  constructor(private fb: FormBuilder, private router: Router, private giftAidStateService: OnboardingGiftAidStateService) {}
+  constructor(private fb: FormBuilder, private router: Router, private giftAidStateService: OnboardingGiftAidStateService,private translationService:TranslateService, private toastr: ToastrService) {}
 
   ngOnInit() {
     const currentSettings = this.getCachedValue();
     this.form = this.fb.group({
       authorisedOfficialHomeAddressLineOne: [
         currentSettings ? currentSettings.authorisedOfficialHomeAddressLineOne : null,
-        [Validators.required, Validators.maxLength(50)]
+        [Validators.required]
       ],
       authorisedOfficialHomeAddressLineTwo: [
         currentSettings ? currentSettings.authorisedOfficialHomeAddressLineTwo : null,
-        [Validators.required, Validators.maxLength(150)]
+        [Validators.required]
       ],
       authorisedOfficialHomeAddressLineThree: [
         currentSettings ? currentSettings.authorisedOfficialHomeAddressLineThree : null,
-        [Validators.required, Validators.maxLength(150)]
+        [Validators.required]
       ],
-      authorisedOfficialHomeAddressLineZipCode: [currentSettings ? currentSettings.authorisedOfficialHomeAddressLineZipCode : null, [Validators.maxLength(15)]],
-      authorisedOfficialHomeAddressLineCountry: [currentSettings ? currentSettings.authorisedOfficialHomeAddressLineCountry : null, [Validators.maxLength(50)]]
+      authorisedOfficialHomeAddressLineZipCode: [currentSettings ? currentSettings.authorisedOfficialHomeAddressLineZipCode : null, [Validators.required]],
+      authorisedOfficialHomeAddressLineCountry: [currentSettings ? currentSettings.authorisedOfficialHomeAddressLineCountry : null, [Validators.required]]
     });
   }
 
@@ -42,8 +46,49 @@ export class GiftaidAuthorisedOfficialAddressDetailsComponent implements OnInit 
   }
 
   public submit():void{
-    // if validated
-    this.continue();
+    if (this.form.invalid) {
+      this.handleInvalidForm();
+      return;
+    } else {
+      this.continue();
+    }
+  }
+
+  handleInvalidForm() {
+    let errorMessages = new Array<Observable<string>>();
+    let resolvedErrorMessages = new Array<string>();
+
+    const addressLine1Errors = this.form.get('authorisedOfficialHomeAddressLineOne').errors;
+    const addressLine2Errors = this.form.get('authorisedOfficialHomeAddressLineTwo').errors;
+    const addressLine3Errors = this.form.get('authorisedOfficialHomeAddressLineThree').errors;
+    const addressZipCodeErrors = this.form.get('authorisedOfficialHomeAddressLineZipCode').errors;
+    const addressCountryErrors = this.form.get('authorisedOfficialHomeAddressLineCountry').errors;
+
+    if (addressLine1Errors && addressLine1Errors.required) {
+      errorMessages.push(this.translationService.get('errorMessages.charity-number-required'));
+    }
+    if (addressLine2Errors && addressLine1Errors.required) {
+      errorMessages.push(this.translationService.get('errorMessages.charity-number-required'));
+    }
+    if (addressLine3Errors && addressLine3Errors.required) {
+      errorMessages.push(this.translationService.get('errorMessages.charity-number-required'));
+    }
+    if (addressZipCodeErrors && addressZipCodeErrors.required) {
+      errorMessages.push(this.translationService.get('errorMessages.charity-number-required'));
+    }
+    if (addressCountryErrors && addressCountryErrors.required) {
+      errorMessages.push(this.translationService.get('errorMessages.charity-number-required'));
+    }
+
+    forkJoin(errorMessages)
+      .pipe(tap(results => (resolvedErrorMessages = results)))
+      .pipe(tap(results => console.log(results)))
+      .pipe(switchMap(() => this.translationService.get('errorMessages.validation-errors')))
+      .subscribe(title =>
+        this.toastr.warning(resolvedErrorMessages.join('<br>'), title, {
+          enableHtml: true
+        })
+      );
   }
 
   // only call this function when all of the input has been validated
@@ -58,7 +103,6 @@ export class GiftaidAuthorisedOfficialAddressDetailsComponent implements OnInit 
 
     this.giftAidStateService.currentGiftAidSettings = currentSettings;
     this.giftAidStateService.validatedAndCompletedStepThree = true;
-    // todo implement the route
     this.router.navigate(['/', 'onboarding','giftaid', {outlets: {'onboarding-outlet': ['verify-organisation-details']}}]);
   }
 }
