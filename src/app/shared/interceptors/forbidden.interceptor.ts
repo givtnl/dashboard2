4dd4fae765435b1cd4e093026d177c38903ef830
Bjorn Derudder <bjorn.derudder@gmail.com>
2020-03-16 15:31:10 +0100

    added toaster

--- src/app/shared/interceptors/forbidden.interceptor.ts
+++ src/app/shared/interceptors/forbidden.interceptor.ts
@@ -3,6 +3,7 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/c
 import { Observable } from 'rxjs';
 import { catchErrorStatus } from 'src/app/shared/extensions/observable-extensions';
 import { Router } from '@angular/router';
+import { TranslatableToastrService } from '../services/translate-able-toastr.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,11 @@ export class ForbiddenInterceptor implements HttpInterceptor {
   /**
    *
    */
-  constructor(private router: Router) {}
+  constructor(private router: Router, private toaster: TranslatableToastrService) {}
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(req).pipe(catchErrorStatus(403, () => this.router.navigate(['/','account','login'])));
+    return next.handle(req).pipe(catchErrorStatus(403, () => {
+        this.router.navigate(['/','account','login'])
+        this.toaster.error("errorMessages.forbidden.title", "errorMessages.forbidden")
+    }));
   }
 }
