df281f5d06bee34201a5b0fc34dfbf1f987ba301
Jonas Brabant <jonas@givtapp.net>
2020-03-19 14:57:57 +0100

    Added visible counter and go automatically to contract form

--- src/app/onboarding/organisation-details/onboarding-organisation-details-parent-known/onboarding-organisation-details-parent-known.component.ts
+++ src/app/onboarding/organisation-details/onboarding-organisation-details-parent-known/onboarding-organisation-details-parent-known.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { ApplicationStateService } from 'src/app/infrastructure/services/application-state.service';
 
 @Component({
   selector: 'app-onboarding-organisation-details-parent-known',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OnboardingOrganisationDetailsParentKnownComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private applicationStateService: ApplicationStateService) { }
 
-  ngOnInit() {
+  private contractUrl: string;
+  private initialTimeLeft: number = 6;
+  public timeLeft: number = this.initialTimeLeft;
+  private interval;
+  public loading = false;
+
+  startTimer() {
+    this.interval = setInterval(() => {
+      if (this.timeLeft > 0) {
+        this.timeLeft--;
+      }
+      else {
+        this.loading = true;
+        window.location.href = this.contractUrl;
+      }
+    }, 1000)
   }
 
+  ngOnInit() {
+    this.contractUrl =`${environment.apiUrl}/contract/organisations/${this.applicationStateService.currentTokenModel.OrganisationAdmin}`
+  }
+
+  ngAfterViewInit() {
+    this.startTimer();
+  }
 }
