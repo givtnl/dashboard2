b9ccc19b00c0d48259d69b1fd12067d1bbeab747
Anthony <info@datafunc.be>
2020-03-27 11:52:13 +0100

    remove ITS THE FINAL COUNTDOWN timer and use new term

--- src/app/onboarding/organisation-details/onboarding-organisation-details-parent-known/onboarding-organisation-details-parent-known.component.ts
+++ src/app/onboarding/organisation-details/onboarding-organisation-details-parent-known/onboarding-organisation-details-parent-known.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { ApplicationStateService } from 'src/app/infrastructure/services/application-state.service';
 import { OnboardingOrganisationDetailsStateService } from '../services/onboarding-organisation-details-state.service';
@@ -10,39 +9,14 @@ import { OnboardingOrganisationDetailsStateService } from '../services/onboardin
   styleUrls: ['../../onboarding.module.scss', './onboarding-organisation-details-parent-known.component.scss']
 })
 export class OnboardingOrganisationDetailsParentKnownComponent implements OnInit {
-  constructor(
-    private router: Router,
-    private onboardingOrganisationDetailStateService: OnboardingOrganisationDetailsStateService,
-    private applicationStateService: ApplicationStateService
-  ) {}
+  constructor(private onboardingOrganisationDetailStateService: OnboardingOrganisationDetailsStateService, private applicationStateService: ApplicationStateService) {}
 
-  private contractUrl: string;
-  private initialTimeLeft: number = 5;
-  public timeLeft: number = this.initialTimeLeft;
-  private interval;
+  public contractUrl: string;
+
   public loading = false;
-
-  startTimer() {
-    this.interval = setInterval(() => {
-      if (this.timeLeft > 0) {
-        this.timeLeft--;
-      } else {
-        this.loading = true;
-        window.location.href = this.contractUrl;
-      }
-    }, 1000);
-  }
 
   ngOnInit() {
     const charityCommissionReference = this.onboardingOrganisationDetailStateService.currentCharityNumber;
     this.contractUrl = `${environment.apiUrl}/contract/organisations/${this.applicationStateService.currentTokenModel.OrganisationAdmin}?charityCommissionReference=${charityCommissionReference}`;
-  }
-
-  ngAfterViewInit() {
-    this.startTimer();
-  }
-
-  ngOnDestroy() {
-    clearInterval(this.interval);
   }
 }
