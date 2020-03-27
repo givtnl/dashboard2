137b92b82debc7fe553ba36b7abf74c92b5562d8
Anthony <info@datafunc.be>
2020-03-27 18:06:26 +0100

    BUILD IN NG PROD :'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):'):')

--- src/app/onboarding/bank-account-signing/models/bank-account-holder-detail.model.ts
+++ src/app/onboarding/bank-account-signing/models/bank-account-holder-detail.model.ts
@@ -1,20 +1,20 @@
 export interface BankAccountHolderDetailModel {
-    Id: string;
-    AccountId: number;
-    FirstName: string;
-    LastName:string;
-    EmailAddress:number;
-    SortCode:string;
-    AccountNumber:string;
-    AccountName:string;
-    OrganisationId: string;
-    OrganisationName:string;
-    OrganisationAdddressLine1: string;
-    OrganisationAdddressLine2: string;
-    OrganisationAdddressLine3: string;
-    OrganisationAdddressLine4: string;
-    OrganisationAdddressLine5: string;
-    OrganisationZipCode:string;
-    InvitationExpiredDateTime: Date;
-    InvitationIsExpired: boolean;
+  Id: string;
+  AccountId: number;
+  FirstName: string;
+  LastName: string;
+  EmailAddress: number;
+  SortCode: string;
+  AccountNumber: string;
+  AccountName: string;
+  OrganisationId: string;
+  OrganisationName: string;
+  OrganisationAddressLine1: string;
+  OrganisationAddressLine2: string;
+  OrganisationAddressLine3: string;
+  OrganisationAddressLine4: string;
+  OrganisationAddressLine5: string;
+  OrganisationZipCode: string;
+  InvitationExpiredDateTime: Date;
+  InvitationIsExpired: boolean;
 }
