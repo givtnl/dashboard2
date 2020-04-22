56c3e0abf55dc859e6da1d0fcb87dc2a264a476c
Anthony <svansay@pansay.be>
2020-04-22 16:37:35 +0200

    start thinking out a model

---
+++ src/app/organisations/models/commands/update-organisation.command.ts
@@ -0,0 +1,12 @@
+export interface UpdateOrganisationCommand {
+    name: string;
+    addressLine1: string;
+    addressLine2: string;
+    addressLine3: string;
+    addressLine4: string;
+    addressLine5: string;
+    // address: string
+    postalCode: string;
+    city: string;
+    country: string;
+}
---
+++ src/app/preboarding/models/preboarding-create-collection-medium-shipment-address.command.ts
@@ -0,0 +1,11 @@
+export interface PreboardingCreateCollectionMediumShipmentAddressCommand {
+    name: string;
+    addressLine1: string;
+    addressLine2: string;
+    addressLine3: string;
+    addressLine4: string;
+    addressLine5: string;
+    postalCode: string;
+    city: string;
+    country: string;
+}
