ae0f7284383e07fcc630d67ef2df1ef2c83b6249
Mike <mike@givtapp.net>
2020-04-23 15:58:10 +0200

    setup routing from details to complete

--- src/app/preboarding/preboarding-organisation-admin-details/preboarding-organisation-admin-details.component.ts
+++ src/app/preboarding/preboarding-organisation-admin-details/preboarding-organisation-admin-details.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { Observable, forkJoin } from 'rxjs';
+import { tap, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-preboarding-organisation-admin-details',
@@ -7,9 +13,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreboardingOrganisationAdminDetailsComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup
+  constructor(
+    private formBuilder: FormBuilder, 
+    private translationService: TranslateService, 
+    private toastr: ToastrService,
+    private router: Router) { }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      organisatorEmail: [null, [Validators.required, Validators.email]]
+    });
   }
+  submit() {
+    if (this.form.invalid) {
+      this.handleInvalidForm();
+      return;
+    }
+    this.router.navigate(["/preboarding/register/complete"])
+  }
+  
+  handleInvalidForm() {
+    let errorMessages = new Array<Observable<string>>();
+    let resolvedErrorMessages = new Array<string>();
 
+    const organisatorEmailErrors = this.form.get('organisatorEmail').errors;
+
+    if (organisatorEmailErrors) {
+      if (organisatorEmailErrors.required) {
+        errorMessages.push(this.translationService.get('errorMessages.organisator-email-required'));
+      }
+      if (organisatorEmailErrors.email) {
+        errorMessages.push(this.translationService.get('errorMessages.email-not-an-email'));
+      }
+    }
+
+    forkJoin(errorMessages)
+      .pipe(tap(results => (resolvedErrorMessages = results)))
+      .pipe(switchMap(results => this.translationService.get('errorMessages.validation-errors')))
+      .subscribe(title =>
+        this.toastr.warning(resolvedErrorMessages.join('<br>'), title, {
+          enableHtml: true
+        })
+      );
+  }
 }
