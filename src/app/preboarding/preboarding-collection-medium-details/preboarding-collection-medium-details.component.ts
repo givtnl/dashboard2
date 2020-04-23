6a420314aa4dbbf0a7b8f11d9c2db6a534356a94
Mike <mike@givtapp.net>
2020-04-23 15:28:15 +0200

    setup routing from address details to medium details

--- src/app/preboarding/preboarding-collection-medium-details/preboarding-collection-medium-details.component.ts
+++ src/app/preboarding/preboarding-collection-medium-details/preboarding-collection-medium-details.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { Observable, forkJoin } from 'rxjs';
+import { tap, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-preboarding-collection-medium-details',
@@ -7,9 +13,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreboardingCollectionMediumDetailsComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup
+  constructor(
+    private formBuilder: FormBuilder, 
+    private translationService: TranslateService, 
+    private toastr: ToastrService,
+    private router: Router) { }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      numberOfVisitors: [null, [Validators.required]],
+      numberOfCollectionBags: [null, [Validators.required]], 
+    });
+  }
+  submit() {
+    if (this.form.invalid) {
+      this.handleInvalidForm();
+      return;
+    }
+    this.router.navigate(["/preboarding/register/organisation-admin-details"])
+  }
+  
+  handleInvalidForm() {
+    let errorMessages = new Array<Observable<string>>();
+    let resolvedErrorMessages = new Array<string>();
+
+    const numberOfVisitorsErrors = this.form.get('numberOfVisitors').errors;
+    const numberOfCollectionBagsErrors = this.form.get('numberOfCollectionBags').errors;
+
+    if (numberOfVisitorsErrors) {
+      if (numberOfVisitorsErrors.required) {
+        errorMessages.push(this.translationService.get('errorMessages.number-of-visitors-required'));
+      }
+    }
+    if (numberOfCollectionBagsErrors) {
+      if (numberOfCollectionBagsErrors.required) {
+        errorMessages.push(this.translationService.get('errorMessages.number-of-collectionbags-required'));
+      }
+    }
+    
+
+    forkJoin(errorMessages)
+      .pipe(tap(results => (resolvedErrorMessages = results)))
+      .pipe(switchMap(results => this.translationService.get('errorMessages.validation-errors')))
+      .subscribe(title =>
+        this.toastr.warning(resolvedErrorMessages.join('<br>'), title, {
+          enableHtml: true
+        })
+      );
   }
 
 }
