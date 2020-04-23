1a605dfa760f8358f01fa1908f05fb71db0e8a80
Anthony <svansay@pansay.be>
2020-04-23 08:56:14 +0200

    add navigation to the next screen

--- src/app/preboarding/preboarding-organisation-address-details/preboarding-organisation-address-details.component.ts
+++ src/app/preboarding/preboarding-organisation-address-details/preboarding-organisation-address-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { PreboardingStateService } from '../services/preboarding-state.service';
 import { notNullOrEmptyValidator } from 'src/app/shared/validators/notnullorempty.validator';
+import { RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-preboarding-organisation-address-details',
@@ -11,7 +12,9 @@ import { notNullOrEmptyValidator } from 'src/app/shared/validators/notnullorempt
 export class PreboardingOrganisationAddressDetailsComponent implements OnInit {
 
   public form: FormGroup;
-  constructor(private fb: FormBuilder, private stateService: PreboardingStateService) { }
+  public loading = false;
+
+  constructor(private fb: FormBuilder, private stateService: PreboardingStateService, private router: Router) { }
 
   ngOnInit() {
     this.form = this.fb.group({
@@ -41,6 +44,8 @@ export class PreboardingOrganisationAddressDetailsComponent implements OnInit {
 
   continue(): void {
     this.stateService.currentOrganisationDetails = this.form.getRawValue();
+    this.loading = true;
+    this.router.navigate(['/', 'preboarding', 'register', 'collection-medium-details']).finally(() => this.loading = false);
   }
 
 }
