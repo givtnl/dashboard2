7f25db28818702e23ab7474737a7ed29ffcb6e3f
Anthony <svansay@pansay.be>
2020-04-23 08:53:53 +0200

    add the form in the just form

--- src/app/preboarding/preboarding-details-complete/preboarding-details-complete.component.ts
+++ src/app/preboarding/preboarding-details-complete/preboarding-details-complete.component.ts
@@ -10,38 +10,10 @@ import { UpdateOrganisationCommand } from 'src/app/organisations/models/commands
   styleUrls: ['./preboarding-details-complete.component.scss']
 })
 export class PreboardingDetailsCompleteComponent implements OnInit {
-
-  public form: FormGroup;
-  constructor(private fb: FormBuilder, private stateService: PreboardingStateService) { }
-
-  ngOnInit() {
-    this.form = this.fb.group({
-      name: [null, [Validators.required, notNullOrEmptyValidator()]],
-      addressLineOne: [null, [Validators.required, notNullOrEmptyValidator()]],
-      addressLineTwo: [null, [Validators.required, notNullOrEmptyValidator()]],
-      addressLineThree: [null],
-      addressLineFour: [null],
-      addressLineFive: [null],
-      postalCode: [null, [Validators.required, notNullOrEmptyValidator()]],
-      country: [null, [Validators.required, notNullOrEmptyValidator()]]
-    });
+  ngOnInit(): void {
+    throw new Error("Method not implemented.");
   }
 
-  submit(): void {
-    if (this.form.invalid) {
-      this.handleInvalidForm();
-      return;
-    } else {
-      this.continue();
-    }
-  }
 
-  handleInvalidForm(): void {
-
-  }
-
-  continue(): void {
-    this.stateService.currentOrganisationDetails = this.form.getRawValue();
-  }
 
 }
--- src/app/preboarding/preboarding-organisation-address-details/preboarding-organisation-address-details.component.ts
+++ src/app/preboarding/preboarding-organisation-address-details/preboarding-organisation-address-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { PreboardingStateService } from '../services/preboarding-state.service';
+import { notNullOrEmptyValidator } from 'src/app/shared/validators/notnullorempty.validator';
 
 @Component({
   selector: 'app-preboarding-organisation-address-details',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreboardingOrganisationAddressDetailsComponent implements OnInit {
 
-  constructor() { }
+  public form: FormGroup;
+  constructor(private fb: FormBuilder, private stateService: PreboardingStateService) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      name: [null, [Validators.required, notNullOrEmptyValidator()]],
+      addressLineOne: [null, [Validators.required, notNullOrEmptyValidator()]],
+      addressLineTwo: [null, [Validators.required, notNullOrEmptyValidator()]],
+      addressLineThree: [null],
+      addressLineFour: [null],
+      addressLineFive: [null],
+      postalCode: [null, [Validators.required, notNullOrEmptyValidator()]],
+      country: [null, [Validators.required, notNullOrEmptyValidator()]]
+    });
+  }
+
+  submit(): void {
+    if (this.form.invalid) {
+      this.handleInvalidForm();
+      return;
+    } else {
+      this.continue();
+    }
+  }
+
+  handleInvalidForm(): void {
+
+  }
+
+  continue(): void {
+    this.stateService.currentOrganisationDetails = this.form.getRawValue();
   }
 
 }
