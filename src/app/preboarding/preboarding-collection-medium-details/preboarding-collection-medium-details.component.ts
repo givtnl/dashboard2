63f09b1b53269d188d75c290ddf8089534fe7230
Mike <mike@givtapp.net>
2020-04-28 11:27:28 +0200

    added red border to select list if nothing selected

--- src/app/preboarding/preboarding-collection-medium-details/preboarding-collection-medium-details.component.ts
+++ src/app/preboarding/preboarding-collection-medium-details/preboarding-collection-medium-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray, ValidatorFn, ValidationErrors } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
 import { ToastrService } from 'ngx-toastr';
 import { Router, ActivatedRoute } from '@angular/router';
@@ -17,6 +17,8 @@ export class PreboardingCollectionMediumDetailsComponent implements OnInit {
 
   public form: FormGroup
   public additionalInformationCommand: CreatePreboardingAdditionalInformationCommand;
+  formSubmitted = false;
+
   constructor(
     private route: ActivatedRoute,
     private formBuilder: FormBuilder,
@@ -24,28 +26,8 @@ export class PreboardingCollectionMediumDetailsComponent implements OnInit {
     private toastr: ToastrService,
     private preboardingStateService: PreboardingStateService,
     private router: Router) {
-
   }
 
-
-  singleCollectionDetails(): FormArray {
-    return this.form.get('singleCollectionDetails') as FormArray;
-  }
-  multipleCollectionDetails(): FormArray {
-    return this.form.get('multipleCollectionDetails') as FormArray;
-  }
-  endOfServiceCollectionDetails(): FormArray {
-    return this.form.get('endOfServiceCollectionDetails') as FormArray;
-  }
-  communionCollectionDetails(): FormArray {
-    return this.form.get('communionCollectionDetails') as FormArray;
-  }
-  candleCollectionDetails(): FormArray {
-    return this.form.get('candleCollectionDetails') as FormArray;
-  }
-  collectionBoxesDetails(): FormArray {
-    return this.form.get('collectionBoxesDetails') as FormArray;
-  }
   ngOnInit() {
     this.additionalInformationCommand = this.route.snapshot.data.additionalInformation;
     console.log(this.additionalInformationCommand.singleCollectionService.details)
@@ -71,30 +53,12 @@ export class PreboardingCollectionMediumDetailsComponent implements OnInit {
       this.additionalInformationCommand.candleCollection.enabled ? 
       (this.mapDetailsToFormArray(this.additionalInformationCommand.candleCollection.details && this.additionalInformationCommand.candleCollection.details.length > 0 ? 
         this.additionalInformationCommand.candleCollection.details : 
-        [this.mapDetail() as PreboardingCollectionDetail])) : null,
-      collectionBoxesDetails: 
-      this.additionalInformationCommand.collectionBoxes.enabled ? 
-      (this.mapDetailsToFormArray(this.additionalInformationCommand.collectionBoxes.details && this.additionalInformationCommand.collectionBoxes.details.length > 0 ? 
-        this.additionalInformationCommand.collectionBoxes.details : 
-        [this.mapDetail() as PreboardingCollectionDetail])) : null,
+        [this.mapDetail() as PreboardingCollectionDetail])) : null
     });
   }
 
-
-  mapDetail(detail: PreboardingCollectionDetail = null): FormGroup {
-    return this.formBuilder.group({
-      quantity: [detail ? detail.quantity : null, [Validators.min(1)]],
-      collectionType: [detail ? detail.collectionType : null, [Validators.required]]
-    })
-  }
-
-  mapDetailsToFormArray(details: PreboardingCollectionDetail[]): FormArray {
-    return this.formBuilder.array(details.map(x => this.mapDetail(x)));
-  }
-
-
-
   submit() {
+    this.formSubmitted = true;
     if (this.form.invalid) {
       this.handleInvalidForm();
       return;
@@ -109,7 +73,6 @@ export class PreboardingCollectionMediumDetailsComponent implements OnInit {
     this.additionalInformationCommand.endOfServiceCollection.details = this.endOfServiceCollectionDetails().value;
     this.additionalInformationCommand.communionCollection.details = this.communionCollectionDetails().value;
     this.additionalInformationCommand.candleCollection.details = this.candleCollectionDetails().value;
-    this.additionalInformationCommand.collectionBoxes.details = this.collectionBoxesDetails().value;
     this.preboardingStateService.currentAdditionalInformation = this.additionalInformationCommand;
   }
 
@@ -141,6 +104,42 @@ export class PreboardingCollectionMediumDetailsComponent implements OnInit {
           enableHtml: true
         })
       );
+  }
+
+  mapDetail(detail: PreboardingCollectionDetail = null): FormGroup {
+    return this.formBuilder.group({
+      quantity: [detail ? detail.quantity : null, [Validators.required, Validators.min(0)]],
+      collectionType: [detail ? detail.collectionType : null, [Validators.required, this.collectionTypeSelectionChanged()]]
+    })
+  }
+
+  mapDetailsToFormArray(details: PreboardingCollectionDetail[]): FormArray {
+    return this.formBuilder.array(details.map(x => this.mapDetail(x)));
+  }
+
+  singleCollectionDetails(): FormArray {
+    return this.form.get('singleCollectionDetails') as FormArray;
+  }
+  multipleCollectionDetails(): FormArray {
+    return this.form.get('multipleCollectionDetails') as FormArray;
+  }
+  endOfServiceCollectionDetails(): FormArray {
+    return this.form.get('endOfServiceCollectionDetails') as FormArray;
+  }
+  communionCollectionDetails(): FormArray {
+    return this.form.get('communionCollectionDetails') as FormArray;
+  }
+  candleCollectionDetails(): FormArray {
+    return this.form.get('candleCollectionDetails') as FormArray;
+  }
+  private collectionTypeSelectionChanged(): ValidatorFn {
+    /* To prevent submitting the form */
+    /* Showing red border around dropdown when invalid happens in html. */
+    return (group: FormGroup): ValidationErrors => {
+      if(group.value == null)
+        return {collectionTypeNotSelected: true}
+      return null;
+    }
   }
 
 }
