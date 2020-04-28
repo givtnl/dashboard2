530baa1837cda4e10999a7018da52e73014943e9
bjornderudder <bjorn.derudder@gmail.com>
2020-04-28 17:18:23 +0200

    Don't link the account with collectgroup after creating it

--- src/app/onboarding/bank-account/guards/onboarding-bank-account-complete-check-success.guard.ts
+++ src/app/onboarding/bank-account/guards/onboarding-bank-account-complete-check-success.guard.ts
@@ -23,20 +23,8 @@ export class OnboardingBankAccountCompleteCheckSuccessGuard implements CanActiva
     try {
       const registration = this.onboardingBankAccountStateService.currentBankAccountModel;
       const currentToken = this.applicationStateService.currentTokenModel;
-
       // create the bank account
-      const createdResponse = await this.onboardingBankAccountService.create(currentToken.OrganisationAdmin, registration).toPromise();
-      // retrieve the collectgroups
-      const collectGroups = await this.collectGroupService.getAll(currentToken.OrganisationAdmin).toPromise();
-
-      // if there is only one collect group, it means its the default, link it with this account we just created
-      if (collectGroups && collectGroups.length === 1 && !collectGroups[0].AccountId) {
-        await this.collectGroupService.addAccountToCollectGroup(
-          currentToken.OrganisationAdmin,
-          createdResponse.Result,
-          collectGroups[0].Id
-        ).toPromise();
-      }
+      await this.onboardingBankAccountService.create(currentToken.OrganisationAdmin, registration).toPromise();
     } catch (error) {
       if (error instanceof HttpErrorResponse && error.status !== 422) {
         return this.HandleFailure(next);
