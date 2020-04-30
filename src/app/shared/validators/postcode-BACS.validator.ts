bc095caabaedcdaf24dc4f70985cffc6ae14ced4
Mike <mike@givtapp.net>
2020-04-30 13:48:38 +0200

    fix postcode null validation error

--- src/app/shared/validators/postcode-BACS.validator.ts
+++ src/app/shared/validators/postcode-BACS.validator.ts
@@ -1,14 +1,14 @@
 import { ValidatorFn, AbstractControl } from '@angular/forms';
+import { isNullOrUndefined } from 'util';
 
 export function postCodeBACSValidator(): ValidatorFn {
   return (control: AbstractControl): { [key: string]: boolean } | null => {
+    if(isNullOrUndefined(control.value)) 
+      return { invalidPostCode: true };
     let enteredValue = control.value.trim();
-    if(enteredValue == "" || !enteredValue.includes(" ") || !isValidBACSPostCode(enteredValue)){
-      return {
-        invalidPostCode: true
-      };
-    }
-    return null;
+    if (enteredValue == "" || !enteredValue.includes(" ") || !isValidBACSPostCode(enteredValue))
+      return { invalidPostCode: true };
+    return null
   };
 }
 
