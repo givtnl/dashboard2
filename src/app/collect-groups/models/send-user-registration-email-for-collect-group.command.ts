1135d7f97d2992f4c2a8bf1dc63ab72dedcddf64
Mike <mike@givtapp.net>
2020-05-01 11:41:23 +0200

    make it nullable do we dont have to fill it in

--- src/app/collect-groups/models/send-user-registration-email-for-collect-group.command.ts
+++ src/app/collect-groups/models/send-user-registration-email-for-collect-group.command.ts
@@ -1,6 +1,6 @@
 export interface SendUserRegistrationEmailForCollectGroupCommand {
     collectGroupId: string;
     email:string;
-    password:string;
+    password?:string;
     language:string;
 }
