0765ae7f4e14f5e69ce38cbaa00af6e9aa4f319a
Mike <mike@givtapp.net>
2020-05-01 11:42:28 +0200

    added 2 method for the qr code part

--- src/app/collect-groups/services/collect-groups.service.ts
+++ src/app/collect-groups/services/collect-groups.service.ts
@@ -5,6 +5,7 @@ import { CollectGroupListModel } from '../models/collect-group-list.model';
 import { CreateCollectGroupCommand } from '../models/create-collect-group.command';
 import { CreatedResponseModel } from 'src/app/infrastructure/models/response.model';
 import { CreateUserForCollectGroupCommand } from '../models/create-user-for-collect-group.command';
+import { HttpParams } from '@angular/common/http';
 @Injectable({
     providedIn:'root'
 })
@@ -24,7 +25,19 @@ export class CollectGroupsService {
     }
 
     // create qr
+    addCollectionMedium( collectGroupId: string ): Observable<CreatedResponseModel<string>>{
+        return this.backendService.post(`v2/collectgroups/${collectGroupId}/collectionmediums`, {})
+    }
     //export qr
+
+    // exportCollectionMedium(collectGroupId: string, collectionMediumId: string, language: string, email: string) : Observable<object>{
+    //    let queryParams = new HttpParams();
+    //    queryParams = queryParams.set('emailAddress', email);
+    //    queryParams = queryParams.set('receiverFirstName',' ');
+    //    queryParams = queryParams.set('templateName', somebuildErrorDajtzoetinvuln);
+       
+    //     return this.backendService.get(`v2/collectgroups/${collectGroupId}/collectionMediums/${collectionMediumId}/export/${language}`, queryParams);
+    // }
 
     addAccountToCollectGroup(organisationId: string, accountId: number, collectGroupId: string): Observable<object>{
         return this.backendService.post(`v2/organisations/${organisationId}/collectgroups/${collectGroupId}/accounts`,{
