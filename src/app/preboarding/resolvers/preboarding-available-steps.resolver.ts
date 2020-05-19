61debb8221874a324b85709408fa0d15be664666
Anthony <svansay@pansay.be>
2020-05-19 09:16:42 +0200

    remove the guard that executes our api calls

---
+++ src/app/preboarding/resolvers/preboarding-available-steps.resolver.ts
@@ -0,0 +1,49 @@
+import { Resolve, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { PreboardingStepListModel } from '../preboarding-details-complete/models/preboarding-step-list.model';
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PreboardingAvailableStepsResolver implements Resolve<PreboardingStepListModel[]> {
+    resolve(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot
+    ): PreboardingStepListModel[] | Observable<PreboardingStepListModel[]> | Promise<PreboardingStepListModel[]> {
+        return [
+            {
+                loading: true,
+                term: 'PreboardingDetailsCompleteComponent.steps-update-organisation',
+            },
+            {
+                loading: true,
+                term: 'PreboardingDetailsCompleteComponent.steps-registering-in-app'
+            },
+            {
+                loading: true,
+                term: 'PreboardingDetailsCompleteComponent.steps-create-collection-mediums'
+            },
+            {
+                loading: true,
+                term: 'PreboardingDetailsCompleteComponent.steps-export-collection-medium'
+            },
+            {
+                loading: true,
+                term: 'PreboardingDetailsCompleteComponent.steps-export-collection-medium-by-mail'
+            },
+            {
+                loading: true,
+                term: 'PreboardingDetailsCompleteComponent.steps-inviting-collect-group-admins'
+            },
+            {
+                loading: true,
+                term: 'PreboardingDetailsCompleteComponent.steps-adding-notes-to-teamleader'
+            },
+            {
+                loading: true,
+                term: 'PreboardingDetailsCompleteComponent.steps-update-teamleader-progress'
+            }];
+    }
+}
