a01cb48cebe4d70a4c34b54d9c5feb34ce4efd61
Anthony <svansay@pansay.be>
2020-05-19 09:16:54 +0200

    remove unneeded injected variable

--- src/app/shared/interceptors/error-term.interceptor.ts
+++ src/app/shared/interceptors/error-term.interceptor.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { Router } from '@angular/router';
 import { catchError } from 'rxjs/operators';
 import { TranslatableToastrService } from '../services/translate-able-toastr.service';
 
@@ -9,7 +8,7 @@ import { TranslatableToastrService } from '../services/translate-able-toastr.ser
   providedIn: 'root'
 })
 export class ErrorTermInterceptor implements HttpInterceptor {
-  constructor(private router: Router, private toaster: TranslatableToastrService) { }
+  constructor(private toaster: TranslatableToastrService) { }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(catchError((result) => {
       this.showErrorTermIfAny(result);
