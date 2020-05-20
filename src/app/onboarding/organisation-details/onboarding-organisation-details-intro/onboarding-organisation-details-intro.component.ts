c84570cc89d8c55dcda5c80220ed2bc8ff8339dd
Mike <mike@givtapp.net>
2020-05-20 15:56:36 +0200

    route to the new screens instead of old contract form

--- src/app/onboarding/organisation-details/onboarding-organisation-details-intro/onboarding-organisation-details-intro.component.ts
+++ src/app/onboarding/organisation-details/onboarding-organisation-details-intro/onboarding-organisation-details-intro.component.ts
@@ -1,20 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { ApplicationStateService } from 'src/app/infrastructure/services/application-state.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-onboarding-organisation-details-intro',
   templateUrl: './onboarding-organisation-details-intro.component.html',
   styleUrls: ['../../onboarding.module.scss', './onboarding-organisation-details-intro.component.scss']
 })
-export class OnboardingOrganisationDetailsIntroComponent implements OnInit {
+export class OnboardingOrganisationDetailsIntroComponent {
+  constructor(
+    private router: Router) { }
 
-  public contractUrl: String;
+  public gotoNextStep() {
+    this.router.navigate(['/', 'onboarding', 'organisation-details', { outlets: { 'onboarding-outlet': ['verify-organisation-name'] } }])
 
-  constructor( private applicationStateService: ApplicationStateService) { 
-  }
-
-  ngOnInit() {
-    this.contractUrl =`${environment.apiUrl}/contract/organisations/${this.applicationStateService.currentTokenModel.OrganisationAdmin}`;
   }
 }
