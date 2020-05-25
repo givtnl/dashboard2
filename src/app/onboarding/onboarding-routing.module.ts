82e7d5c574bdc0c751827802db805e0f74e977a1
Anthony <svansay@pansay.be>
2020-05-25 17:46:03 +0200

    reactivate disabled guard

--- src/app/onboarding/onboarding-routing.module.ts
+++ src/app/onboarding/onboarding-routing.module.ts
@@ -124,7 +124,7 @@ const routes: Routes = [
   {
     path: 'organisation-details',
     component: OnboardingRootComponent,
-    // canActivate: [AuthenticationGuard],
+   canActivate: [AuthenticationGuard],
     children: [
       {
         path: '',
