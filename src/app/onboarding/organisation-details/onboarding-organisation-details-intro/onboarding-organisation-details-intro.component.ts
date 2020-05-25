23d170df0dcb2b897b263f03be0fa0f559f1b20d
Mike <mike@givtapp.net>
2020-05-25 14:31:19 +0200

    change routing to component backend and set isManualRegistration true

--- src/app/onboarding/organisation-details/onboarding-organisation-details-intro/onboarding-organisation-details-intro.component.ts
+++ src/app/onboarding/organisation-details/onboarding-organisation-details-intro/onboarding-organisation-details-intro.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { OnboardingOrganisationDetailsStateService } from '../services/onboarding-organisation-details-state.service';
 
 @Component({
   selector: 'app-onboarding-organisation-details-intro',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['../../onboarding.module.scss', './onboarding-organisation-details-intro.component.scss']
 })
 export class OnboardingOrganisationDetailsIntroComponent {
-  constructor() { }
+  constructor(private router: Router, private service: OnboardingOrganisationDetailsStateService) { }
+  
+  gotoManualEntry() {
+    this.service.isManualRegistration = true
+    this.router.navigate(['/', 'onboarding', 'organisation-details', { outlets: { 'onboarding-outlet': ['verify-organisation-name'] } }], {
+      queryParamsHandling: 'merge'
+    })
+  }
+
 }
