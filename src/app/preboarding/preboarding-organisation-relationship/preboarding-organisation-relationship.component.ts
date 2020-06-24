e1712e99a5f176dbdc3706f07ede383deeae1ba1
Anthony <svansay@pansay.be>
2020-06-24 10:22:03 +0200

    add finishing touches to the MODAL html

--- src/app/preboarding/preboarding-organisation-relationship/preboarding-organisation-relationship.component.ts
+++ src/app/preboarding/preboarding-organisation-relationship/preboarding-organisation-relationship.component.ts
@@ -3,7 +3,6 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 import { OrganisationWithRulesDetail } from 'src/app/onboarding/organisation-details/models/organisation-with-rules-detail.model';
 import { PreboardingStateService } from '../services/preboarding-state.service';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-preboarding-organisation-relationship',
@@ -13,14 +12,11 @@ import { TranslateService } from '@ngx-translate/core';
 
 export class PreboardingOrganisationRelationComponent implements OnInit {
 
-
-    public loading = false;
     public form: FormGroup;
-
     public providingOrganisations: OrganisationWithRulesDetail[] = [];
 
-    constructor(private activatedRoute: ActivatedRoute, private formBuilder: FormBuilder, private preBoardingStateService: PreboardingStateService, private router: Router, private translationService: TranslateService ) {
-        
+    constructor(private activatedRoute: ActivatedRoute, private formBuilder: FormBuilder, private preBoardingStateService: PreboardingStateService, private router: Router) {
+
     }
 
     ngOnInit(): void {
@@ -31,37 +27,29 @@ export class PreboardingOrganisationRelationComponent implements OnInit {
             ],
             rules: this.formBuilder.array([])
         });
-        /* Bjorn */ this.providingOrganisations.unshift({
-            Id: null,
-            Name: this.translationService.instant("preboardingOrganisationRelationComponent.notApplicable"),
-            RelationshipRules: []
-        })
-        /* Anthony */ this.form.get('providingOrganisation').valueChanges.subscribe(newValue => this.preFillRules(newValue));
+        this.providingOrganisations = this.activatedRoute.snapshot.data.providingOrganisations;
+        this.form.get('providingOrganisation').valueChanges.subscribe(newValue => this.preFillRules(newValue));
     }
 
     submit() {
-        let currentOrganisationRelationship = this.form.get('providingOrganisationId') as FormArray;
-        if (currentOrganisationRelationship.value != null)
-            this.preBoardingStateService.organisationRelationship = this.activatedRoute.snapshot.data.providingOrganisations.find(org => org.Id == currentOrganisationRelationship.value);
-        else
-            this.preBoardingStateService.organisationRelationship = null
-        this.router.navigate(["/preboarding/register/mail-box-address-details"])
+        this.preBoardingStateService.organisationRelationship = this.form.value.providingOrganisation;
+        this.router.navigate(["/preboarding/register/mail-box-address-details"]);
     }
 
     rules(): FormArray {
         return this.form.get('rules') as FormArray;
     }
-
+f
     preFillRules(organisation: OrganisationWithRulesDetail): void {
         this.rules().clear();
-
         organisation.RelationshipRules.forEach(rule => {
             this.rules().push(this.formBuilder.group({
+                ruleType: rule.RelationshipType,
+                optional: rule.Optional,
                 selected: [{
-                    value: !rule.Optional,
-                    disabled:!rule.Optional
+                    value: true,
+                    disabled: !rule.Optional
                 }]
-              
             }))
         })
     }
