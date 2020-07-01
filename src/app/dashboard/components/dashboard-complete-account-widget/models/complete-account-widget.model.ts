ef54869a240b51cdf988c9cf2e3395b2d6f90573
Maarten Vergouwe <mvergouwe85@gmail.com>
2020-07-01 10:12:51 +0200

    Use proper enum

--- src/app/dashboard/components/dashboard-complete-account-widget/models/complete-account-widget.model.ts
+++ src/app/dashboard/components/dashboard-complete-account-widget/models/complete-account-widget.model.ts
@@ -1,7 +1,9 @@
+import { OrganisationRegistrationStatus } from '../enums/organisationregistrationstatus.enum';
+
 export interface CompleteAccountWidgetModel {
-    DisplayOrder:number;
-    Finished:boolean;
-    InProgress:boolean;
-    OrganisationRegistrationStatus:number;
-    OrganisationRegistrationStatusDescription:string;
+    DisplayOrder: number;
+    Finished: boolean;
+    InProgress: boolean;
+    OrganisationRegistrationStatus: OrganisationRegistrationStatus;
+    OrganisationRegistrationStatusDescription: string;
 }
