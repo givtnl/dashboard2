648d241075e86c5d57213155f8ee26fe4ef92e72
Maarten Vergouwe <maarten@givtapp.net>
2021-04-13 11:34:51 +0200

    Get CollectGroups from CollectgroupService

--- src/app/shared/services/dashboard.service.ts
+++ src/app/shared/services/dashboard.service.ts
@@ -4,6 +4,10 @@ import { BackendService } from 'src/app/infrastructure/services/backend.service'
 import { Observable } from 'rxjs';
 import { CollectGroupDashboardListModel } from '../models/collect-group-side-bar-list.model';
 import { EventEmitter } from '@angular/core';
+import { CollectGroupsService } from 'src/app/collect-groups/services/collect-groups.service';
+import { ApplicationStateService } from 'src/app/infrastructure/services/application-state.service';
+import { map } from 'rxjs/operators';
+import { CollectGroupType } from '../enums/collect-group-type.enum';
 
 
 @Injectable({
@@ -25,10 +29,21 @@ export class DashboardService {
         this.currentCollectGroupChange.emit(collectGroup);
     }
         
-    constructor(private backendService: BackendService) { }
+    constructor(private backendService: BackendService,
+        private collectGroupsService: CollectGroupsService,
+        private applicationStateService: ApplicationStateService
+        ) { }
 
     public getCollectGroups(): Observable<CollectGroupDashboardListModel[]> {
-        return this.backendService
-            .get<CollectGroupDashboardListModel[]>('collectgroupview/collectgroup');
+        return this.collectGroupsService.getAll(this.applicationStateService.currentTokenModel.OrganisationAdmin)
+            .pipe(map(x => x.map(y => {
+                const a: CollectGroupDashboardListModel = {
+                    GUID: y.Id,
+                    CollectGroupType: y.Type,
+                    CollectGroupTypeDescription: CollectGroupType[y.Type],
+                    Name: y.Name
+                };
+                return a;
+            })));
     }
 }
