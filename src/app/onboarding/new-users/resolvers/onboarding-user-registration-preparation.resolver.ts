ba0fd29c37313a57cf7f157215be113209507870
Maarten Vergouwe <maarten@givtapp.net>
2021-05-18 16:45:58 +0200

    catch duplicate entry and route to login page

--- src/app/onboarding/new-users/resolvers/onboarding-user-registration-preparation.resolver.ts
+++ src/app/onboarding/new-users/resolvers/onboarding-user-registration-preparation.resolver.ts
@@ -1,27 +1,29 @@
 import { Resolve, RouterStateSnapshot, ActivatedRouteSnapshot, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { UserRegistrationResponseModel } from '../models/user-registration-response.model';
 import { catchErrorStatus } from 'src/app/shared/extensions/observable-extensions';
 import { OnboardingNewUsersService } from '../services/onboarding-new-users.service';
+import { map } from 'rxjs/operators';
 
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class OnboardingUserRegistrationPreparationResolver implements Resolve<UserRegistrationResponseModel> {
-  /**
-   *
-   */
-  constructor(private service: OnboardingNewUsersService, private router: Router) {}
-  resolve(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): UserRegistrationResponseModel | Observable<UserRegistrationResponseModel> | Promise<UserRegistrationResponseModel> {
-    const collectGroupId = route.queryParams.collectGroupId as string;
-    const emailAddress = route.queryParams.emailAddress as string;
-    return this.service
-      .prepareUser(collectGroupId, emailAddress)
-      .pipe(catchErrorStatus(401, () => this.router.navigate(['/', 'system-module', 'not-found'])));
-  }
+    /**
+     *
+     */
+    constructor(private service: OnboardingNewUsersService, private router: Router) { }
+    resolve(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot
+    ): UserRegistrationResponseModel | Observable<UserRegistrationResponseModel> | Promise<UserRegistrationResponseModel> {
+        const collectGroupId = route.queryParams.collectGroupId as string;
+        const emailAddress = route.queryParams.emailAddress as string;
+        return this.service
+            .prepareUser(collectGroupId, emailAddress)
+            .pipe(catchErrorStatus(401, () => this.router.navigate(['/', 'system-module', 'not-found'])))
+            .pipe(catchErrorStatus(409, () => this.router.navigate(['/'])));
+    }
 }
