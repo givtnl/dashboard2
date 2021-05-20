7f9eb945cf400c777bbc5fc861c03fa93542c820
Maarten Vergouwe <maarten@givtapp.net>
2021-05-20 17:28:07 +0200

    Revert "Revert "Allow for showing an overview of registered dashboard users""
    This reverts commit 5b323e80e635fea1d94452c50631b7b5ceb063da.

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -11,6 +11,7 @@ import { DashboardLinkButtonComponent } from './components/dashboard-link-button
 import { CollectGroupDashboardHomeComponent } from './collect-group-dashboard-home/collect-group-dashboard-home.component';
 import { CollectGroupDashboardPeopleComponent } from './collect-group-dashboard-people/collect-group-dashboard-people.component';
 import { DashboardSelectOrganisationComponent } from './select-organisation/select-organisation.component';
+import { DashboardUsersComponent } from './dashboard-users/dashboard-users.component';
 @NgModule({
     declarations: [
         CollectGroupDashboardHomeComponent,
@@ -20,7 +21,8 @@ import { DashboardSelectOrganisationComponent } from './select-organisation/sele
         DashboardCompleteAccountWidgetComponent,
         DashboardTileWidgetComponent,
         DashboardLinkButtonComponent,
-        DashboardSelectOrganisationComponent
+        DashboardSelectOrganisationComponent,
+        DashboardUsersComponent
     ],
     imports: [ DashboardRoutingModule, CommonModule, ReactiveFormsModule, SharedModule ]
 })
---
+++ src/app/users/models/dashboard-user-detail.model.ts
@@ -0,0 +1,5 @@
+export interface DashboardUserDetailModel {
+    FirstName: string;
+    LastName: string;
+    EmailAddress: string;
+}
