27ad7c1577d608414be33915007d72a7e16c3cc1
Maarten Vergouwe <maarten@givtapp.net>
2021-06-03 13:48:25 +0200

    Use correct organisationAdmin reference

--- src/app/dashboard/resolvers/dashboard-user-invites.resolver.ts
+++ src/app/dashboard/resolvers/dashboard-user-invites.resolver.ts
@@ -3,13 +3,14 @@ import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/r
 import { Observable } from "rxjs";
 import { OrganisationUserInviteListModel } from "src/app/dashboard-user-registration/models/organisation-user-invite-list.model";
 import { OrganisationUserInviteService } from "src/app/dashboard-user-registration/services/organisation-user-invite.service";
+import { ApplicationStateService } from "src/app/infrastructure/services/application-state.service";
 
 @Injectable({
     'providedIn':'root'
 }) export class DashboardUserInvitesResolver implements Resolve<OrganisationUserInviteListModel[]> {
-    constructor(private service: OrganisationUserInviteService) { }
+    constructor(private service: OrganisationUserInviteService, private applicationStateService: ApplicationStateService) { }
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): OrganisationUserInviteListModel[] | Observable<OrganisationUserInviteListModel[]> | Promise<OrganisationUserInviteListModel[]> {
-        return this.service.getAll(route.queryParams.organisationId);
+        return this.service.getAll(this.applicationStateService.currentTokenModel.OrganisationAdmin);
     }
 }
