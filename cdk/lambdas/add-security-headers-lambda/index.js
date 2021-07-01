bd890f3785ee2b84dc0b71644e4b18c5a26e7b13
Anthony <svansay@pansay.com>
2021-07-01 12:06:49 +0200

    add mixpanel to the trusted sources

--- cdk/lambdas/add-security-headers-lambda/index.js
+++ cdk/lambdas/add-security-headers-lambda/index.js
@@ -8,7 +8,7 @@ exports.handler = (event, context, callback) => {
     //Set new headers 
     headers['permissions-policy'] = [{ key: 'Permissions-Policy', value: `fullscreen=(self);` }]
     headers['strict-transport-security'] = [{ key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubdomains; preload' }];
-    headers['content-security-policy'] = [{ key: 'Content-Security-Policy', value: "connect-src 'self' givt-debug-api.azurewebsites.net api.givtapp.net www.google-analytics.com; default-src 'none';font-src 'self' use.fontawesome.com cdnjs.cloudflare.com; img-src 'self' www.google-analytics.com; script-src cdnjs.cloudflare.com code.jquery.com stackpath.bootstrapcdn.com www.gstatic.com www.googletagmanager.com www.google-analytics.com 'unsafe-eval' 'self'; style-src 'unsafe-inline' 'self' use.fontawesome.com cdnjs.cloudflare.com; object-src 'none';" }];
+    headers['content-security-policy'] = [{ key: 'Content-Security-Policy', value: "connect-src 'self' api-js.mixpanel.com givt-debug-api.azurewebsites.net api.givtapp.net www.google-analytics.com; default-src 'none';font-src 'self' use.fontawesome.com cdnjs.cloudflare.com; img-src 'self' www.google-analytics.com; script-src cdnjs.cloudflare.com code.jquery.com stackpath.bootstrapcdn.com www.gstatic.com www.googletagmanager.com www.google-analytics.com 'unsafe-eval' 'self'; style-src 'unsafe-inline' 'self' use.fontawesome.com cdnjs.cloudflare.com; object-src 'none';" }];
     headers['x-content-type-options'] = [{ key: 'X-Content-Type-Options', value: 'nosniff' }];
     headers['x-frame-options'] = [{ key: 'X-Frame-Options', value: 'DENY' }];
     headers['x-xss-protection'] = [{ key: 'X-XSS-Protection', value: '1; mode=block' }];
