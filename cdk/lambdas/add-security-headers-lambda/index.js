15966dda06d99e9cbdc907e7bdb9ee107dff0df6
phalox <toon@phalox.be>
2022-12-30 08:40:51 +0100

    wildcarded wepay domain for security policy

--- cdk/lambdas/add-security-headers-lambda/index.js
+++ cdk/lambdas/add-security-headers-lambda/index.js
@@ -10,14 +10,14 @@ exports.handler = (event, context, callback) => {
     //Set new headers 
     headers['permissions-policy'] = [{ key: 'Permissions-Policy', value: `fullscreen=(self);` }]
     headers['strict-transport-security'] = [{ key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubdomains; preload' }];
-    var connectSrc = "connect-src 'self' api-js.mixpanel.com givt-debug-api.azurewebsites.net api.givtapp.net givt-debug-api-us.azurewebsites.net api.givt.app www.google-analytics.com cdn.wepay.com t.wepay.com;";
+    var connectSrc = "connect-src 'self' api-js.mixpanel.com givt-debug-api.azurewebsites.net api.givtapp.net givt-debug-api-us.azurewebsites.net api.givt.app www.google-analytics.com *.wepay.com;";
     var defaultSrc = "default-src 'none';";
-    var fontSrc = "font-src 'self' fonts.gstatic.com use.fontawesome.com cdnjs.cloudflare.com cdn.wepay.com t.wepay.com;";
-    var imgSrc = "img-src 'self' data: www.google-analytics.com cdn.wepay.com t.wepay.com;";
-    var scriptSrc = "script-src 'self' cdnjs.cloudflare.com code.jquery.com stackpath.bootstrapcdn.com cdn.wepay.com t.wepay.com www.gstatic.com www.googletagmanager.com www.google-analytics.com 'unsafe-eval' 'self';";
+    var fontSrc = "font-src 'self' fonts.gstatic.com use.fontawesome.com cdnjs.cloudflare.com *.wepay.com;";
+    var imgSrc = "img-src 'self' data: www.google-analytics.com *.wepay.com;";
+    var scriptSrc = "script-src 'self' cdnjs.cloudflare.com code.jquery.com stackpath.bootstrapcdn.com *.wepay.com www.gstatic.com www.googletagmanager.com www.google-analytics.com 'unsafe-eval' 'self';";
     var styleSrc = "style-src 'unsafe-inline' 'self' use.fontawesome.com cdnjs.cloudflare.com;";
     var objectSrc = "object-src 'none';";
-    var frameSrc = "frame-src 'self' cdn.wepay.com t.wepay.com;";
+    var frameSrc = "frame-src 'self' *.wepay.com;";
     headers['content-security-policy'] = [{ key: 'Content-Security-Policy', value: `${connectSrc} ${defaultSrc} ${fontSrc} ${imgSrc} ${scriptSrc} ${styleSrc} ${objectSrc} ${frameSrc}` }];
     headers['x-content-type-options'] = [{ key: 'X-Content-Type-Options', value: 'nosniff' }];
     headers['x-frame-options'] = [{ key: 'X-Frame-Options', value: 'DENY' }];
