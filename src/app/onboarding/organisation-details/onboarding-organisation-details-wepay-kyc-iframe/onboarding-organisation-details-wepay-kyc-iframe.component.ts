5c21205df78f80d7055f00a0a46273afba08dda8
Mike Pattyn <mike.pattyn1@gmail.com>
2023-02-02 07:32:13 +0100

    Use the wepay iframe

--- src/app/onboarding/organisation-details/onboarding-organisation-details-wepay-kyc-iframe/onboarding-organisation-details-wepay-kyc-iframe.component.ts
+++ src/app/onboarding/organisation-details/onboarding-organisation-details-wepay-kyc-iframe/onboarding-organisation-details-wepay-kyc-iframe.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from "@angular/core";
 import { Router } from "@angular/router";
 import { Subject } from "rxjs";
 import { finalize, takeUntil, tap } from "rxjs/operators";
@@ -6,6 +6,14 @@ import { environment } from "src/environments/environment";
 import { OnboardingOrganisationDetailsService } from "../services/onboarding-organisation-details.service";
 import { DashboardService } from "src/app/shared/services/dashboard.service";
 import { TranslatableToastrService } from "src/app/shared/services/translate-able-toastr.service";
+import { AngularWePayService } from "@givtnl/angular-wepay-service";
+import { WePay } from "@givtnl/angular-wepay-service/lib/types";
+
+const apiVersion = "3.0";
+const kycIframeLabel = "#kycIframe"
+const iframe_container_id = "kyc-iframe";
+const country = "US";
+const ssn4_enabled = true;
 
 @Component({
   selector: "app-onboarding-organisation-details-wepay-kyc-iframe",
@@ -16,8 +24,7 @@ import { TranslatableToastrService } from "src/app/shared/services/translate-abl
   ],
 })
 export class OnboardingOrganisationDetailsWePayIframeComponent
-  implements OnInit, OnDestroy
-{
+  implements OnInit, OnDestroy, AfterViewInit {
   loading: boolean = false;
   hasConfigurationError: boolean = false;
   hasCreationError: boolean = false;
@@ -25,65 +32,44 @@ export class OnboardingOrganisationDetailsWePayIframeComponent
   environment = environment.production ? "production" : "stage";
   apiVersion = "3.0";
   private ngUnsubscribe = new Subject<void>();
+  @ViewChild(kycIframeLabel, { static: false }) public iframeContainer!: ElementRef;
+  wepay: WePay;
 
   constructor(
     private router: Router,
     private onboardingOrganisationDetailsService: OnboardingOrganisationDetailsService,
     private dashboardService: DashboardService,
-    private toastr: TranslatableToastrService
-  ) {}
+    private toastr: TranslatableToastrService,
+    private wepayService: AngularWePayService
+  ) { }
 
   ngOnInit(): void {
-    this.initialiseIframe();
-    this.initIframeLoader();
+    this.wepay = this.wepayService.wepay;
+    this.iframeLoading = false;
   }
 
-  initialiseIframe() {
-    var error = window["WePay"].configure(
-      this.environment,
-      environment.wePayAppId,
-      this.apiVersion
-    );
-    if (error) {
-      this.hasConfigurationError = true;
-      return;
-    }
-    var iframe_container_id = "kyc-iframe";
-    var country = "US";
-    var ssn4_enabled = true;
-    var kycIframe = window["WePay"].createKycIframe(iframe_container_id, {
-      country_code: country.toUpperCase(),
-      ssn4_enabled: ssn4_enabled,
-    });
-    if (kycIframe.error_code) {
-      this.hasCreationError = true;
-      return;
-    }
-    this.initialiseEventListenerOnSubmitBtn(kycIframe);
-  }
-
-  initialiseEventListenerOnSubmitBtn(kycIframe){
+  initialiseEventListenerOnSubmitBtn(kycIframe) {
     document
-    .getElementById("submit-kyc-button")
-    .addEventListener("click", (event) => {
-      this.loading = true;
-      kycIframe
-        .tokenize()
-        .then((response) => {
-          this.handleSubmitButtonClick(response);
-        })
-        .catch((_)=> {
-          this.showErrorMessage();
-          this.loading = false;
-        });
-    });
+      .getElementById("submit-kyc-button")
+      .addEventListener("click", (event) => {
+        this.loading = true;
+        kycIframe
+          .tokenize()
+          .then((response) => {
+            this.handleSubmitButtonClick(response);
+          })
+          .catch((_) => {
+            this.showErrorMessage();
+            this.loading = false;
+          });
+      });
   }
 
-  handleSubmitButtonClick(token){
+  handleSubmitButtonClick(token) {
     this.loading = true;
     let kycToken = token.id;
     let organisationId = this.dashboardService.currentOrganisation.Id
-    this.onboardingOrganisationDetailsService.saveWePayKYCDetails(kycToken,organisationId).pipe(
+    this.onboardingOrganisationDetailsService.saveWePayKYCDetails(kycToken, organisationId).pipe(
       takeUntil(this.ngUnsubscribe),
       finalize(() => {
         this.loading = false;
@@ -104,13 +90,6 @@ export class OnboardingOrganisationDetailsWePayIframeComponent
     );
   }
 
-  initIframeLoader(){
-    setTimeout(()=>{
-      this.iframeLoading = false;
-    },1500)
-
-  }
-
   async showErrorMessage() {
     this.toastr.warning('OnboardingOrganisationDetailsWePayComponent.submitErrorTitle', 'OnboardingOrganisationDetailsWePayComponent.submitErrorSubTitle')
   }
@@ -118,5 +97,70 @@ export class OnboardingOrganisationDetailsWePayIframeComponent
   ngOnDestroy() {
     this.ngUnsubscribe.next();
     this.ngUnsubscribe.complete();
+  }
+
+  ngAfterViewInit() {
+
+    // Styling options documented here: https://dev.wepay.com/clear/cookbooks/style-credit-card-iframes/
+    const custom_style = {
+      'styles': {
+        'cvv-icon': {
+          'base': {
+            'display': 'none'
+          }
+        },
+        'base': {
+          'height': '44px',
+          'margin': '0',
+          'border-radius': '4px',
+          'font-family': 'Avenir',
+          'font-size': '16px',
+          'color': '#2C2B57',
+          '::placeholder': {
+            'color': '#BCB9C8'
+          },
+          ':focus': {
+            'border': '1px solid #2C2B57'
+          }
+        },
+        'invalid': {
+          'border': '2px solid #D73C49'
+        },
+        'valid': {
+          'border': '1px solid #41C98E'
+        },
+        'errors': {
+          'invalid': {
+            'color': '#D73C49'
+          }
+        }
+      }
+    };
+    // iframe options documented here: https://dev.wepay.com/clear/cookbooks/style-credit-card-iframes/
+    const options = {
+      custom_style: custom_style,
+      show_labels: false,
+      show_placeholders: true,
+      show_error_messages: false,
+      show_error_messages_when_unfocused: false,
+      country_code: country.toUpperCase(),
+      ssn4_enabled: ssn4_enabled
+    };
+    let error = this.wepay.configure(
+      environment.production ? "production" : "stage",
+      environment.wePayAppId, apiVersion);
+
+    if (error) {
+      console.log(error)
+    };
+
+    var kycIframe = window['WePay'].createKycIframe(iframe_container_id, options);
+
+    if (kycIframe.error_code) {
+      this.hasCreationError = true;
+      return;
+    }
+
+    this.initialiseEventListenerOnSubmitBtn(kycIframe);
   }
 }
