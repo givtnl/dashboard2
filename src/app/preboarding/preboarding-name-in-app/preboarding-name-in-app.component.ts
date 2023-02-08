56cfb1f91cb19cf98432153272e225ee533ff7f2
gkavanagh <113672096+gkavanagh@users.noreply.github.com>
2023-02-08 12:26:04 +0200

    added validation to UI for input

--- src/app/preboarding/preboarding-name-in-app/preboarding-name-in-app.component.ts
+++ src/app/preboarding/preboarding-name-in-app/preboarding-name-in-app.component.ts
@@ -12,97 +12,140 @@ import { UniqueCollectGroupNameValidator } from 'src/app/collect-groups/validato
 import { CollectGroupsService } from 'src/app/collect-groups/services/collect-groups.service';
 
 @Component({
-    selector: 'app-preboarding-name-in-app',
-    templateUrl: './preboarding-name-in-app.component.html',
-    styleUrls: ['./preboarding-name-in-app.component.scss', '../../preboarding/preboarding.module.scss']
+  selector: "app-preboarding-name-in-app",
+  templateUrl: "./preboarding-name-in-app.component.html",
+  styleUrls: [
+    "./preboarding-name-in-app.component.scss",
+    "../../preboarding/preboarding.module.scss",
+  ],
 })
-
 export class PreboardingNameInAppComponent implements OnInit, OnDestroy {
-    private ngUnsubscribe = new Subject<void>();
-    public form: FormGroup
-    public questionMarkPicturePath: string;
+  private ngUnsubscribe = new Subject<void>();
+  public form: FormGroup;
+  public questionMarkPicturePath: string;
 
-    private collectGroup: CreateCollectGroupCommand;
+  private collectGroup: CreateCollectGroupCommand;
 
-    constructor(
-        private route: ActivatedRoute,
-        private collectGroupService: CollectGroupsService,
-        private formBuilder: FormBuilder,
-        private translationService: TranslateService,
-        private toastr: ToastrService,
-        private preboardingStateService: PreboardingStateService,
-        private router: Router) { }
+  constructor(
+    private route: ActivatedRoute,
+    private collectGroupService: CollectGroupsService,
+    private formBuilder: FormBuilder,
+    private translationService: TranslateService,
+    private toastr: ToastrService,
+    private preboardingStateService: PreboardingStateService,
+    private router: Router
+  ) {}
 
-    ngOnInit() {
-        this.collectGroup = this.route.snapshot.data.collectGroup;
-        this.form = this.formBuilder.group({
-            inAppOrgName: [this.collectGroup ? this.collectGroup.name : null, [Validators.required, Validators.maxLength(30), notNullOrEmptyValidator()],
-            [UniqueCollectGroupNameValidator.create(this.collectGroupService)]],
-            paymentReference: [this.preboardingStateService.organisationDetails.country.toLowerCase() === 'nl' ? 'Automatische betaling Givt' : 'Automatic Payment Givt']
-        });
-        switch (this.translationService.currentLang) {
-            case "nl":
-                this.questionMarkPicturePath = "../../../../assets/images/organisation-name-example1-nl.png"
-                break;
-            default:
-                this.questionMarkPicturePath = "../../../../assets/images/organisation-name-example1-en.png"
-        }
-        // Set up a word count underneath the input text field
-        document.getElementById('count_message').innerText = `0/30`;
-        this.form.get('inAppOrgName').valueChanges
-        .pipe(takeUntil(this.ngUnsubscribe))
-        .subscribe(text=>{
-            document.getElementById('count_message').innerText = `${text.length}/30`;
+  ngOnInit() {
+    this.collectGroup = this.route.snapshot.data.collectGroup;
+    this.form = this.formBuilder.group({
+      inAppOrgName: [
+        this.collectGroup ? this.collectGroup.name : null,
+        [
+          Validators.required,
+          Validators.maxLength(30),
+          notNullOrEmptyValidator(),
+        ],
+        [UniqueCollectGroupNameValidator.create(this.collectGroupService)],
+      ],
+      paymentReference: [
+        this.preboardingStateService.organisationDetails.country.toLowerCase() ===
+        "nl"
+          ? "Automatische betaling Givt"
+          : "Automatic Payment Givt",
+      ],
+    });
+    switch (this.translationService.currentLang) {
+      case "nl":
+        this.questionMarkPicturePath =
+          "../../../../assets/images/organisation-name-example1-nl.png";
+        break;
+      default:
+        this.questionMarkPicturePath =
+          "../../../../assets/images/organisation-name-example1-en.png";
+    }
+    // Set up a word count underneath the input text field
+    document.getElementById("count_message").innerText = `0/30`;
+    this.form
+      .get("inAppOrgName")
+      .valueChanges.pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe((text) => {
+        document.getElementById(
+          "count_message"
+        ).innerText = `${text.length}/30`;
+      });
+  }
+
+  isFormControlValid(control: string): boolean {
+    if (this.form.get(control).pristine) {
+      return true;
+    }
+    if (this.form.get(control).valid) {
+      return true;
+    }
+    if (this.form.status === "PENDING") {
+      return true;
+    }
+
+    return false;
+  }
+
+  submit() {
+    if (this.form.invalid) {
+      this.handleInvalidForm();
+      return;
+    }
+    this.continue();
+    this.router.navigate(["/preboarding/register/relationship"]);
+  }
+
+  continue() {
+    this.collectGroup.name = this.form.value.inAppOrgName.trim();
+    this.collectGroup.paymentReference = this.form.value.paymentReference;
+    this.preboardingStateService.currentCollectGroupDetails = this.collectGroup;
+  }
+
+  handleInvalidForm() {
+    let errorMessages = new Array<Observable<string>>();
+    let resolvedErrorMessages = new Array<string>();
+
+    const inAppOrgNameErrors = this.form.get("inAppOrgName").errors;
+
+    if (inAppOrgNameErrors) {
+      if (inAppOrgNameErrors.trimEmptyValue || inAppOrgNameErrors.required) {
+        errorMessages.push(
+          this.translationService.get("errorMessages.name-in-app-required")
+        );
+      }
+      if (inAppOrgNameErrors.maxlength) {
+        errorMessages.push(
+          this.translationService.get("errorMessages.name-in-app-length")
+        );
+      }
+      if (inAppOrgNameErrors.unique) {
+        errorMessages.push(
+          this.translationService.get("errorMessages.unique-collectgroup-name")
+        );
+      }
+    }
+
+    forkJoin(errorMessages)
+      .pipe(
+        takeUntil(this.ngUnsubscribe),
+        tap((results) => (resolvedErrorMessages = results)),
+        switchMap((_) =>
+          this.translationService.get("errorMessages.validation-errors")
+        )
+      )
+      .subscribe((title) =>
+        this.toastr.warning(resolvedErrorMessages.join("<br>"), title, {
+          enableHtml: true,
         })
-    }
+      );
+  }
 
-    submit() {
-        if (this.form.invalid) {
-            this.handleInvalidForm();
-            return;
-        }
-        this.continue();
-        this.router.navigate(["/preboarding/register/relationship"])
-    }
-
-    continue() {
-        this.collectGroup.name = this.form.value.inAppOrgName.trim();
-        this.collectGroup.paymentReference = this.form.value.paymentReference;
-        this.preboardingStateService.currentCollectGroupDetails = this.collectGroup;
-    }
-
-    handleInvalidForm() {
-        let errorMessages = new Array<Observable<string>>();
-        let resolvedErrorMessages = new Array<string>();
-
-        const inAppOrgNameErrors = this.form.get('inAppOrgName').errors;
-
-        if (inAppOrgNameErrors) {
-            if (inAppOrgNameErrors.trimEmptyValue || inAppOrgNameErrors.required) {
-                errorMessages.push(this.translationService.get('errorMessages.name-in-app-required'));
-            }
-            if (inAppOrgNameErrors.maxlength) {
-                errorMessages.push(this.translationService.get('errorMessages.name-in-app-length'));
-            }
-            if (inAppOrgNameErrors.unique) {
-                errorMessages.push(this.translationService.get('errorMessages.unique-collectgroup-name'));
-            }
-        }
-
-        forkJoin(errorMessages)
-            .pipe(
-                takeUntil(this.ngUnsubscribe),
-                tap(results => (resolvedErrorMessages = results)),
-                switchMap(_ => this.translationService.get('errorMessages.validation-errors')))
-            .subscribe(title =>
-                this.toastr.warning(resolvedErrorMessages.join('<br>'), title, {
-                    enableHtml: true
-                })
-            );
-    }
-
-    ngOnDestroy() {
-        this.ngUnsubscribe.next();
-        this.ngUnsubscribe.complete();
-    }
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
 }
